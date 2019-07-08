48f3eebe6b8c4da35c508b682800bc402177a891
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-07-08 13:45:47 +0200

    Added tests
    Added some basic tests to check if the project compiles and NUnit runs

--- restapi.test/restapi_AlwaysFails.cs
+++ restapi.test/restapi_AlwaysFails.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 namespace Tests
 {
-    public class Tests
+    public class AlwaysFails
     {
         [SetUp]
         public void Setup()
@@ -12,7 +12,7 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Assert.Fail();
         }
     }
 }
