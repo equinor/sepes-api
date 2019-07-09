b5fd7b6f58946cf25a1922f0dbdaf2559db1ad5b
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-07-09 12:19:37 +0200

    Added missing package
    Added missing package that stopped tests from being able to initialise the controllers. And updated namespaces to hopefully be more intuitive

--- restapi.test/restapi_AlwaysFails.cs
+++ restapi.test/restapi_AlwaysFails.cs
@@ -1,6 +1,6 @@
 using NUnit.Framework;
 
-namespace Tests
+namespace Sepes.Tests
 {
     public class AlwaysFails
     {
--- restapi/Controllers/ValuesController.cs
+++ restapi/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace restapi.Controllers
+namespace Sepes.restapi.ValuesController
 {
     [Route("api/[controller]")]
     [ApiController]
