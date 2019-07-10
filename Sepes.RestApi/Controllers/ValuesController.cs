15f234507a92aa147f543d6067738d491d3de5e9
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-07-10 13:13:19 +0200

    Implemented requested changes
    New namespace structure
    Removed unecesarry code
    Removed unecesarry tests

--- Sepes.RestApi/Controllers/ValuesController.cs
+++ Sepes.RestApi/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Sepes.restapi.ValuesController
+namespace Sepes.RestApi.Controller
 {
     [Route("api/[controller]")]
     [ApiController]
