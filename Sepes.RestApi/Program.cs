03f5fea109db3a3d0eedd5b0c66cc03373af1678
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-07-10 13:13:19 +0200

    Implemented requested changes
    New namespace structure
    Removed unecesarry code
    Removed unecesarry tests

--- Sepes.RestApi/Program.cs
+++ Sepes.RestApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace restapi
+namespace Sepes.RestApi
 {
     public class Program
     {
