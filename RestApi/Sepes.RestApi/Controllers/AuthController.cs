282349c14499a8232f8e2b7b2135c3b53d334a92
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-08-07 14:33:31 +0200

    Code cleanup
    Cleaned up some pieces of the code

---
+++ RestApi/Sepes.RestApi/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace Sepes.RestApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+    }
+
+}
