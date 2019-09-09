3e9941f8ec0f38cbb08b9e982e0761cbb0b12f68
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-09-09 15:08:43 +0200

    Added pseudocode
    Added pseudocode and suggestion for possible division of controllers

---
+++ RestApi/Sepes.RestApi/Controllers/MembersOrUsersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace Sepes.RestApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MembersController : ControllerBase
+    {
+
+    }
+
+}
---
+++ RestApi/Sepes.RestApi/Controllers/StudyController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace Sepes.RestApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class StudyController : ControllerBase
+    {
+        //Create study
+        [HttpPost("create")]
+        public void Post([FromBody] string value)
+        {
+            /*
+            Unpack data from browser
+            Verify selections are member that exist
+            if this is true then
+                add study to database
+            else
+                return=Invalidselection
+            end
+            
+             */
+        }
+        //Get list of studies
+        [HttpGet("list")]
+        public void Get([FromBody] string value)
+        {
+            //Read from database and return list of current studies.
+        }
+    }
+
+}
