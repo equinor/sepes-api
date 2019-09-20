6f275d622c2bce774bac63918cf2da349c98458a
Kristoffer <kristoffer.ardal@gmail.com>
2019-09-20 16:48:05 +0200

    Added fetch for data

--- FrontEnd/src/sepes.js
+++ FrontEnd/src/sepes.js
@@ -4,4 +4,9 @@ export default {
     getSponsorList: () => ["Tom Andre", "Bjørn Kristiansen", "Ole Martin", "Kristoffer Årdal"],
 
     getDatasetList: () => ["Gudrun", "Heidrun", "Vale", "Volve", "Snøhvit", "Troll", "Sleipner", "Gullfaks", "Goliat", "Kvitebjørn"], 
+
+    getData: async () => {
+        const data = await fetch("https://localhost:5001/api/study/list");
+        return await data.json();
+    }
 }
