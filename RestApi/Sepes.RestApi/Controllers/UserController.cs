94feeca909de6d715f288781fbae519af2d3a60e
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-09-25 16:23:21 +0200

    Added usercontroller and updated database model

--- RestApi/Sepes.RestApi/Controllers/UserController.cs
+++ RestApi/Sepes.RestApi/Controllers/UserController.cs
@@ -6,22 +6,44 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using Sepes.RestApi.Model;
+
 
 namespace Sepes.RestApi.Controller
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("_myAllowSpecificOrigins")]
     [Authorize]
     public class UserController : ControllerBase
     {
-        // GET api/values
-        [HttpGet("list")]
-        public ActionResult<IEnumerable<string>> Get()
+        public IConfiguration Configuration { get; set; }
+        private SepesDb sepesDb = new SepesDb();
+
+        [HttpPost("create")]
+        public int CreationVars([FromBody] User value)
         {
-            throw new NotImplementedException("User list not implemented yet.");
+            return sepesDb.createUser(value);
         }
+
+        [HttpPost("update")]
+        public void UpdateVars([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("list")]
+        public JObject Get()
+        {
+            throw new NotImplementedException("Function not yet implemented, use search api instead");
+            //return sepesDb.getUserList(); //May not be implemented or might be a limited search
+        }
+        //TODO view action
     }
 
 }
