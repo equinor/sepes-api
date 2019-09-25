1c2f6bdbdfd598d219fe35b15283e9ad3d2c7d43
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-09-25 16:07:26 +0200

    New entries for sepesDB

--- RestApi/Sepes.RestApi/Model/Study.cs
+++ RestApi/Sepes.RestApi/Model/Study.cs
@@ -1,11 +1,11 @@
 namespace Sepes.RestApi.Model
 {
 
-public class Study
-{
-    public string studyName { get; set; }
-    public int[] userIds { get; set; }
-    public int[] datasetIds  {get; set; }
-}
+    public class Study
+    {
+        public string studyName { get; set; }
+        public int[] userIds { get; set; }
+        public int[] datasetIds { get; set; }
+    }
 
 }
