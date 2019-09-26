f30a321c80b29b4d255959354ef26b4cb7518156
Kristoffer Årdal <kristoffer.ardal@gmail.com>
2019-09-26 06:52:16 +0200

    integrated create study page with backend

--- FrontEnd/src/sepes.js
+++ FrontEnd/src/sepes.js
@@ -1,11 +1,67 @@
-export default {
-    getSupplierList: () => ["Tom Andre", "Bjørn Kristiansen", "Ole Martin", "Kristoffer Årdal"],
+export default class Sepes {
+    newStudy = {
+        studyName: "New study",
+        userIds: [],
+        datasetIds: []
+    }
 
-    getSponsorList: () => ["Tom Andre", "Bjørn Kristiansen", "Ole Martin", "Kristoffer Årdal"],
+    getSupplierList = () => [{UserId: 1, UserName: "Ricardo Frame", UserEmail: "ricardo@sepes.com"}, 
+                            {UserId: 2, UserName: "Gudrun Draugstad", UserEmail: "gudrun@sepes.com"}, 
+                            {UserId: 3, UserName: "Arne Bjarne Bløffen", UserEmail: "arne@sepes.com"}, 
+                            {UserId: 4, UserName: "Ivy Sadler", UserEmail: "ivy@sepes.com"}, 
+                            {UserId: 5, UserName: "Lilly-May Hill", UserEmail: "lilly@sepes.com"}, 
+                            {UserId: 6, UserName: "Gordon Macfarlane", UserEmail: "gordon@sepes.com"}, 
+                            {UserId: 7, UserName: "Batman", UserEmail: "batman@batman.bat"},]
 
-    getDatasetList: () => ["Gudrun", "Heidrun", "Vale", "Volve", "Snøhvit", "Troll", "Sleipner", "Gullfaks", "Goliat", "Kvitebjørn"], 
+    getSponsorList = () => this.getSupplierList();
 
-    getData: async () => {
+    getDatasetList = () => [{DatasetId: 1 , DatasetName: "Gudrun"}, 
+                            {DatasetId: 2 , DatasetName: "Heidrun"}, 
+                            {DatasetId: 3 , DatasetName: "Vale"}, 
+                            {DatasetId: 4 , DatasetName: "Volve"}, 
+                            {DatasetId: 5 , DatasetName: "Snøhvit"}, 
+                            {DatasetId: 6 , DatasetName: "Troll"}, 
+                            {DatasetId: 7 , DatasetName: "Sleipner"}, 
+                            {DatasetId: 8 , DatasetName: "Gullfaks"}, 
+                            {DatasetId: 9 , DatasetName: "Goliat"}, 
+                            {DatasetId: 10 , DatasetName: "Kvitebjørn"}];
+
+    getData = async () => {
         return await fetch("https://localhost:5001/api/study/list").then(data => data.json());
     }
+
+    setStudyName = (name) => {
+        this.newStudy.studyName = name;
+    }
+
+    createStudy = () => {
+        fetch(process.env.REACT_APP_SEPES_BASE_URL+"/api/study/create", {
+          method: "post",
+          headers: { 
+            "Content-Type": "application/json", 
+            "Authorization": "Bearer " + localStorage.getItem("SepesJWT"),
+          },
+          body: JSON.stringify(this.newStudy)
+        });
+      }
+    
+      addItemToStudy = (id, listName) => {
+        switch(listName) {
+          case "datasetIds": this.newStudy.datasetIds.push(id); break;
+          case "userIds": this.newStudy.userIds.push(id); break;
+          default: break;
+        }
+      }
+    
+      removeItemFromStudy = (id, listName) => {
+        switch(listName) {
+          case "datasetIds": remove(this.newStudy.datasetIds); break;
+          case "userIds": remove(this.newStudy.userIds); break;
+          default: break;
+        }
+        
+        function remove(array) {
+          array.splice(array.indexOf(id), 1);
+        }
+      }
 }
