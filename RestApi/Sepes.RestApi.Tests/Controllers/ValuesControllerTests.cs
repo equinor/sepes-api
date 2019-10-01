fdd437f585d9147d908facb0e5cffb0c2eca27e9
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-10-01 16:11:33 +0200

    Converted to xUnit
    nUnit and xUnit are quite similar so conversion was simple to do.

--- RestApi/Sepes.RestApi.Tests/Controllers/ValuesControllerTests.cs
+++ RestApi/Sepes.RestApi.Tests/Controllers/ValuesControllerTests.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using NUnit.Framework;
+using Xunit;
 using Sepes.RestApi.Controller; //Add the controller to this test
 using System.Linq; //Adds some mathematics
 
@@ -9,14 +9,14 @@ namespace Sepes.RestApi.Tests.Controller
 
     public class ValuesControllerTests
     {
-        [Test]
+        [Fact]
         public void AddNumbers()
         {
             var controller = new ValuesController(); //Make the controller object
             ActionResult<int> result = controller.AddNumbers(2, 3); //Call funtion and pass in variables
 
 
-            Assert.AreEqual(5, result.Value);
+            Assert.Equal(5, result.Value);
 
         }
     }
