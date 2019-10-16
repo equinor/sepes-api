cf3566237f7e2972219b43a5b489da3d20d9d6e4
Kristoffer <kristoffer.ardal@gmail.com>
2019-10-16 16:02:21 +0200

    added archived study

--- RestApi/Sepes.RestApi/Model/Study.cs
+++ RestApi/Sepes.RestApi/Model/Study.cs
@@ -4,8 +4,10 @@ namespace Sepes.RestApi.Model
     public class Study
     {
         public string studyName { get; set; }
+        public int studyId { get; set; }
         public int[] userIds { get; set; }
         public int[] datasetIds { get; set; }
+        public readonly bool archived;
     }
 
 }
