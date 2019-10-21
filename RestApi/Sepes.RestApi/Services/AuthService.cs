ae0b82b50d11a66225c0c3c207e75df4cb46fae1
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-10-21 16:06:30 +0200

    Fixed small issues

--- RestApi/Sepes.RestApi/Services/AuthService.cs
+++ RestApi/Sepes.RestApi/Services/AuthService.cs
@@ -16,28 +16,14 @@ namespace Sepes.RestApi.Services
             _appSetting = appsettings.Value;
         }
 
-        public Task<string> GenerateJSONWebToken(string AZtoken, string OldSepesToken)
+        public Task<string> GenerateJSONWebToken(string token)
         {
-
-            if (OldSepesToken != null)
-            {
-                //Issue: 38 Logic for bringing transplanted traits into new token
-            }
-            else if (AZtoken != null)
-            {
-                //Issue: 38 Logic for any transplanting traits from AzToken to SepesToken
-            }
-            else
-            {
-                //Throw exception as this code should never be reached
-                throw new ArgumentNullException("Both arguments can not be null");
-            }
             var JwtKey = _appSetting.Key;
             var securityKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(JwtKey));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
             //Issue: 38 check if we can add in proper issuer and audience now. or if that has to be done at a later date. Likely going to be part of integration.
-            var token = new JwtSecurityToken(_appSetting.Issuer,  //Issuer
+            var newtoken = new JwtSecurityToken(_appSetting.Issuer,  //Issuer
             _appSetting.Issuer,//Audience  
             null,
             //Issue: 38 suggested that a user is added to token for logging, also check how we can get user to be appended to actions performed in api
@@ -45,7 +31,7 @@ namespace Sepes.RestApi.Services
             expires: DateTime.Now.AddMinutes(120),
             signingCredentials: credentials);
 
-            return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
+            return Task.FromResult(new JwtSecurityTokenHandler().WriteToken(newtoken));
         }
     }
 }
