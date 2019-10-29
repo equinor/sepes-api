f935507e9b8123bb2ebb09c0b9034e16d22a8ac5
Kristoffer <kristoffer.ardal@gmail.com>
2019-10-29 11:04:09 +0100

    Refactored service
    Create study method now uses new study from arg instead of using local member of the service.
    Removed local study member and associated methods.
    Removed dummy data.

--- FrontEnd/src/sepes.js
+++ FrontEnd/src/sepes.js
@@ -1,34 +1,4 @@
 export default class Sepes {
-    newStudy = {
-        studyName: "New study",
-        userIds: [],
-        datasetIds: [],
-        archived: false,
-    };
-
-    getSupplierList = () => [{UserId: 1, UserName: "Ricardo Frame", UserEmail: "ricardo@sepes.com"}, 
-                            {UserId: 2, UserName: "Gudrun Draugstad", UserEmail: "gudrun@sepes.com"}, 
-                            {UserId: 3, UserName: "Arne Bjarne Bløffen", UserEmail: "arne@sepes.com"}, 
-                            {UserId: 4, UserName: "Ivy Sadler", UserEmail: "ivy@sepes.com"}, 
-                            {UserId: 5, UserName: "Lilly-May Hill", UserEmail: "lilly@sepes.com"}, 
-                            {UserId: 6, UserName: "Gordon Macfarlane", UserEmail: "gordon@sepes.com"}, 
-                            {UserId: 7, UserName: "Batman", UserEmail: "batman@batman.bat"},];
-
-    getSponsorList = () => this.getSupplierList();
-
-    getDatasetList = () => [{DatasetId: 1 , DatasetName: "Gudrun"}, 
-                            {DatasetId: 2 , DatasetName: "Heidrun"}, 
-                            {DatasetId: 3 , DatasetName: "Vale"}, 
-                            {DatasetId: 4 , DatasetName: "Volve"}, 
-                            {DatasetId: 5 , DatasetName: "Snøhvit"}, 
-                            {DatasetId: 6 , DatasetName: "Troll"}, 
-                            {DatasetId: 7 , DatasetName: "Sleipner"}, 
-                            {DatasetId: 8 , DatasetName: "Gullfaks"}, 
-                            {DatasetId: 9 , DatasetName: "Goliat"}, 
-                            {DatasetId: 10 , DatasetName: "Kvitebjørn"}];
-    GetDummyPodList = () => [{PodId: 0, PodName: "Testpod"},
-                            {PodId: 1, PodName: "Goliat Data Refinement"},
-                            {PodId: 2, PodName: "Snakeoil"},]
 
     getData = () => {
         return fetch(process.env.REACT_APP_SEPES_BASE_URL+"/api/study/dataset");
@@ -41,28 +11,15 @@ export default class Sepes {
         return fetch(process.env.REACT_APP_SEPES_BASE_URL+"/api/study/list");
     }
 
-    initStudy = () => {
-      this.newStudy = {
-          studyName: "New study",
-          userIds: [],
-          datasetIds: [],
-          archived: false,
-      };
-    }
-
-    setStudyName = (name) => {
-        this.newStudy.studyName = name;
-    }
-
-    createStudy = () => {
-        console.log("Create study: "+this.newStudy.studyName+" - "+this.newStudy.datasetIds.length+" dataset");
+    createStudy = (study) => {
+        console.log("Create study: "+study.studyName+" - "+study.datasetIds.length+" dataset");
         return fetch(process.env.REACT_APP_SEPES_BASE_URL+"/api/pod/create", {
           method: "post",
           headers: { 
             "Content-Type": "application/json", 
             "Authorization": "Bearer " + localStorage.getItem("SepesJWT"),
           },
-          body: JSON.stringify(this.newStudy)
+          body: JSON.stringify(study)
         });
     }
 
@@ -81,17 +38,6 @@ export default class Sepes {
     getPods = (studyId) => {
         return fetch(process.env.REACT_APP_SEPES_BASE_URL+"/api/pod/list/"+studyId);
     }
-    
-    addItemToStudy = (id, listName) => {
-        this.newStudy[listName].push(id);
-        console.log(this.newStudy[listName]);
-    }
-    
-    removeItemFromStudy = (id, listName) => {
-        this.newStudy[listName].splice(this.newStudy[listName].indexOf(id), 1);
-        console.log(this.newStudy[listName]);
-    }
-
 
     updateStudy(studyId, archived) {
       fetch(process.env.REACT_APP_SEPES_BASE_URL+"/api/study/update", {
