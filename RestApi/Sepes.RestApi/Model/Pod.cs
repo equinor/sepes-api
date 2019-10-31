b36238ef12816fe8d779bb79050304d4df1cae0d
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-10-31 16:38:01 +0100

    Cleaning up unused code

--- RestApi/Sepes.RestApi/Model/Pod.cs
+++ RestApi/Sepes.RestApi/Model/Pod.cs
@@ -22,25 +22,5 @@ namespace Sepes.RestApi.Model
         public string resourceGroupName => $"{studyId}-{name.Replace(" ", "-")}-ResourceGroup";
         public string networkSecurityGroupName => $"{studyId}-{name.Replace(" ", "-")}-NetworkSecurityGroup";
         public string addressSpace => $"10.{1 + id / 256}.{id % 256}.0/24";
-
-        // if true ignore port rules and just allow all incoming and outgoing traffick.
-        public readonly bool allowAll;
-        // The ushort is the port. And the uint is the IPv4 address. We will limit us to IPv4 for the beta.
-        // This is the list of ports from the outside that is allowed to talk to the servers inside the pod.
-        // But only from certant ip addresses.
-        public readonly ImmutableDictionary<ushort, ImmutableList<IPAddress>> inGoing;
-        // Same as InComming but now its what ports and ip addresses on the internet servers in a pod can talk to.
-        public readonly ImmutableDictionary<ushort, ImmutableList<IPAddress>> outGoing;
-        // The users that have access to changes the azure resources for this pod.
-        // Will most likely com from the study list of users.
-        public readonly ImmutableList<User> users;
-        // List of datasets that have their rules applied to this pod.
-        // Servers in this pod do not have access to the data at this point.
-        // and DataSets may be removed.
-        public readonly ImmutableList<DataSet> locked;
-        // List of datasets loaded in this pods.
-        // Just as with loaded the rules apply.
-        // You can't remove a dataset from the loaded list.
-        public readonly ImmutableList<DataSet> loaded;
     }
 }
