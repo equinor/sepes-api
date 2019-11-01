c912d0640c22560b5ab02fad39d3fbc607f2be5c
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-01 16:55:09 +0100

    Added some tests for study service
    Tests for rules, userlist and dataset

---
+++ FrontEnd/src/tests/studyService.test.js
@@ -0,0 +1,60 @@
+import * as StudyService from "../studyService"
+
+
+it("should add unique rule to list", () => {
+    let array = [{port: 1, ip: "1.1.1.1"}, {port: 2, ip: "1.1.1.1"}]
+    let rule = {port: 3, ip: "1.1.1.1"}
+
+    let result = StudyService.addRule(rule.port, rule.ip, array)
+    let expected = [{port: 1, ip: "1.1.1.1"}, {port: 2, ip: "1.1.1.1"}, rule]
+    
+    expect(result).toEqual(expected)
+})
+
+it("should not add non-unique rule to list", () => {
+    let array = [{port: 1, ip: "1.1.1.1"}, {port: 2, ip: "1.1.1.1"}]
+    let rule = {port: 1, ip: "1.1.1.1"}
+
+    let result = StudyService.addRule(rule.port, rule.ip, array)
+    let expected = array
+    
+    expect(result).toEqual(expected)
+})
+
+it("should remove rule from list", () => {
+    let list = [{port: 1, ip: "1.1.1.1"}, {port: 2, ip: "1.1.1.1"}]
+    let rule = {port: 2, ip: "1.1.1.1"}
+
+    let result = StudyService.removeRule(rule.port, rule.ip, list)
+    let expected = [{port: 1, ip: "1.1.1.1"}]
+    
+    expect(result).toEqual(expected)
+})
+
+it("should add valid user to list", () => {
+    let list = ["user1", "user2"]
+    let result = StudyService.addUser("user3", list)
+
+    expect(result).toEqual(["user1", "user2", "user3"])
+})
+
+it("should add valid user to list", () => {
+    let list = ["user1", "user2"]
+    let result = StudyService.removeUser("user2", list)
+
+    expect(result).toEqual(["user1"])
+})
+
+it("should add dataset to list as strings", () => {
+    let list = ["dataset1"]
+    let result = StudyService.addDataset({DatasetId: 1, DatasetName: "dataset2"}, list)
+
+    expect(result).toEqual(["dataset1", "dataset2"])
+})
+
+it("should remove dataset from list", () => {
+    let list = ["dataset1", "dataset2"]
+    let result = StudyService.removeDataset("dataset2", list)
+
+    expect(result).toEqual(["dataset1"])
+})
