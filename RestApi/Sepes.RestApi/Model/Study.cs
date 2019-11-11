fb650b2a828257c1a27fe59d8a2084f39f6d2544
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-11 14:22:34 +0100

    Updated study model

--- RestApi/Sepes.RestApi/Model/Study.cs
+++ RestApi/Sepes.RestApi/Model/Study.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Sepes.RestApi.Model
 {
 
@@ -5,8 +7,13 @@ namespace Sepes.RestApi.Model
     {
         public string studyName { get; set; }
         public int studyId { get; set; }
+        public List<Pod> pods { get; set; }
+        public List<string> owners { get; set; }
+        public List<string> users { get; set; }
+        public bool archived { get; set; }
+
+        // old model
         public int[] userIds { get; set; }
         public int[] datasetIds { get; set; }
-        public bool archived { get; set; }
     }
 }
--- RestApi/Sepes.RestApi/Model/StudyInput.cs
+++ RestApi/Sepes.RestApi/Model/StudyInput.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Sepes.RestApi.Model
 {
 
@@ -5,6 +7,11 @@ namespace Sepes.RestApi.Model
     {
         public string studyName { get; set; }
         public int studyId { get; set; }
+        public List<Pod> pods { get; set; }
+        public List<string> owners { get; set; }
+        public List<string> users { get; set; }
+
+        // old model
         public int[] userIds { get; set; }
         public int[] datasetIds { get; set; }
     }
