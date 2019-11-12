cf596cc284839450defcd1612672978910230b71
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-12 16:10:33 +0100

    Changed to immutable vars

--- RestApi/Sepes.RestApi/Model/Study.cs
+++ RestApi/Sepes.RestApi/Model/Study.cs
@@ -1,19 +1,36 @@
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace Sepes.RestApi.Model
 {
 
     public class Study
     {
-        public string studyName { get; set; }
-        public int studyId { get; set; }
-        public List<Pod> pods { get; set; }
-        public List<string> owners { get; set; }
-        public List<string> users { get; set; }
-        public bool archived { get; set; }
+        public readonly string studyName;
+        public readonly int studyId;
+        public readonly ImmutableHashSet<Pod> pods;
+        public readonly ImmutableHashSet<User> sponsors;
+        public readonly ImmutableHashSet<User> suppliers;
+        public readonly ImmutableHashSet<DataSet> datasets;
+        public readonly bool archived;
 
         // old model
-        public int[] userIds { get; set; }
-        public int[] datasetIds { get; set; }
+        public readonly int[] userIds;
+        public readonly int[] datasetIds;
+
+        public Study(string studyName, int studyId, IEnumerable<Pod> pods, IEnumerable<User> sponsors, 
+                    IEnumerable<User> suppliers, IEnumerable<DataSet> datasets, bool archived, int[] userIds, int[] datasetIds)
+        {
+            this.studyName = studyName;
+            this.studyId = studyId;
+            this.pods = pods.ToImmutableHashSet();
+            this.sponsors = sponsors.ToImmutableHashSet();
+            this.suppliers = suppliers.ToImmutableHashSet();
+            this.datasets = datasets.ToImmutableHashSet();
+            this.archived = archived;
+            this.userIds = userIds;
+            this.datasetIds = datasetIds;
+        }
+
     }
 }
