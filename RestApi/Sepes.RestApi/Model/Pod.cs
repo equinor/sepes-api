8640d854c179a039ce7b1c4cca471bebb58d83f1
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-12 16:06:00 +0100

    Added ImmutableLists

--- RestApi/Sepes.RestApi/Model/Pod.cs
+++ RestApi/Sepes.RestApi/Model/Pod.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Net;
 
@@ -6,15 +7,28 @@ namespace Sepes.RestApi.Model
     //Internal version of pod model
     public class Pod
     {
-        public ushort id { get; }
-        public string name { get; }
-        public int studyId { get; }
+        public readonly ushort id;
+        public readonly string name;
+        public readonly int studyId;
+        public readonly bool allowAll;
+        public readonly ImmutableList<Rule> incoming;
+        public readonly ImmutableList<Rule> outgoing;
+        public readonly ImmutableList<User> users;
+        public readonly ImmutableList<DataSet> locked;
+        public readonly ImmutableList<DataSet> loaded;
 
-        public Pod(ushort id, string name, int studyId)
+        public Pod(ushort id, string name, int studyId, bool allowAll, IEnumerable<Rule> incoming, IEnumerable<Rule> outgoing, 
+                    IEnumerable<User> users, IEnumerable<DataSet> locked, IEnumerable<DataSet> loaded)
         {
             this.id = id;
             this.name = name;
             this.studyId = studyId;
+            this.allowAll = allowAll;
+            this.incoming = incoming.ToImmutableList();
+            this.outgoing = outgoing.ToImmutableList();
+            this.users = users.ToImmutableList();
+            this.locked = locked.ToImmutableList();
+            this.loaded = loaded.ToImmutableList();
         }
 
         public string networkName => $"{studyId}-{name.Replace(" ", "-")}-Network";
