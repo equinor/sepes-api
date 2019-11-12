fef99f124abf25b97b4eccb9b8c4deb84c337f4f
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-12 10:10:06 +0100

    Added equals method

--- RestApi/Sepes.RestApi/Model/Study.cs
+++ RestApi/Sepes.RestApi/Model/Study.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sepes.RestApi.Model
 {
@@ -15,5 +17,24 @@ namespace Sepes.RestApi.Model
         // old model
         public int[] userIds { get; set; }
         public int[] datasetIds { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            return obj is Study study &&
+                   studyName == study.studyName &&
+                   studyId == study.studyId &&
+                   Enumerable.SequenceEqual(userIds, study.userIds) &&
+                   Enumerable.SequenceEqual(datasetIds, study.datasetIds) &&
+                   Enumerable.SequenceEqual(pods, study.pods) &&
+                   Enumerable.SequenceEqual(owners, study.owners) &&
+                   Enumerable.SequenceEqual(users, study.users) &&
+                   archived == study.archived;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(studyName, studyId, pods, owners, users, archived, userIds, datasetIds);
+        }
     }
 }
