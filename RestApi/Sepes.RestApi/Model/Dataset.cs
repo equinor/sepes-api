a45d5df067f4de78cb0b00f49ab0c74552bf716d
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-13 15:07:27 +0100

    Add constructor and equals method

--- RestApi/Sepes.RestApi/Model/Dataset.cs
+++ RestApi/Sepes.RestApi/Model/Dataset.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Sepes.RestApi.Model
 {
     // Read only view of the current or proposed state of a Pod.
@@ -5,10 +7,30 @@ namespace Sepes.RestApi.Model
     public class DataSet
     {
         // Name to show user.
-        public readonly string DisplayName;
+        public readonly string displayName;
         // Link to the policy file or document.
-        public readonly string OpaPolicy;
+        public readonly string opaPolicy;
         // Reference to where the data is stored in azure.
-        public readonly string AzureReference;
+        public readonly string azureReference;
+
+        public DataSet(string name, string opaPolicy, string azureReference)
+        {
+            this.displayName = name;
+            this.opaPolicy = opaPolicy;
+            this.azureReference = azureReference;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DataSet dataset &&
+                   displayName == dataset.displayName &&
+                   opaPolicy == dataset.opaPolicy &&
+                   azureReference == dataset.azureReference;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(displayName, opaPolicy, azureReference);
+        }
     }
 }
