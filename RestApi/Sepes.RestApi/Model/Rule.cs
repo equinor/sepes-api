4c3497b0cef773933bdc7afa668d8058b2c2fdf1
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-13 15:06:54 +0100

    Added equals method

--- RestApi/Sepes.RestApi/Model/Rule.cs
+++ RestApi/Sepes.RestApi/Model/Rule.cs
@@ -1,16 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 
 namespace Sepes.RestApi.Model
 {
     public class Rule
     {
+        public readonly ushort port;
+        public readonly IPAddress ip;
+
         public Rule(ushort port, IPAddress ip)
         {
             this.port = port;
             this.ip = ip;
         }
 
-        public readonly ushort port;
-        public readonly IPAddress ip;
+        public override bool Equals(object obj)
+        {
+            return obj is Rule rule &&
+                   port == rule.port &&
+                   ip.Equals(rule.ip);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(port, ip);
+        }
     }
 }
