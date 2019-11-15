65efdafa5460c1938515b27ab9ec6f700b479b9e
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-15 11:04:20 +0100

    Update StudyTest.cs

--- RestApi/Sepes.RestApi.Tests/Model/StudyTest.cs
+++ RestApi/Sepes.RestApi.Tests/Model/StudyTest.cs
@@ -67,34 +67,20 @@ namespace Sepes.RestApi.Tests.Model
         [Fact]
         public void TestEqualityForConversions()
         {
-            var user1 = new User("", "test@test.com", "");
-
             var rule1 = new Rule(8080, IPAddress.Parse("1.1.1.1"));
             var rule2 = new Rule(400, IPAddress.Parse("1.1.1.1"));
-
-            var dataset1 = new DataSet("test", "/test", "twqt4yhqe.qe5w.ywyw5ywq.yq4e5yqe5y");
-            var dataset2 = new DataSet("test2", "/test2", "twqt4yhqe");
-
             var rules = new List<Rule>();
             rules.Add(rule1);
             rules.Add(rule2);
 
-            var users = new List<User>();
-            users.Add(user1);
-
-            var datasets = new List<DataSet>();
-            datasets.Add(dataset1);
-            datasets.Add(dataset2);
-
-            var pod = new Pod(11, "test", 1, false, rules, rules, users, null, null);
+            var pod = new Pod(11, "test", 1, false, rules, rules, null, null, null);
             var pods = new List<Pod>();
             pods.Add(pod);
 
-            Assert.True(pod.Equals(pod.ToPodInput().ToPod()));
-
-            var study1 = new Study("Test-Study", 1, pods, users, users, new List<DataSet>(), false, new int[]{1, 2, 3}, new int[]{1, 2, 3});
+            var study1 = new Study("Test-Study", 1, pods, null, null, new List<DataSet>(), false, new int[]{1, 2, 3}, new int[]{1, 2, 3});
             var study2 = study1.ToStudyInput();
-            var study3 = new Study("Test-Study", 2, pods, users, users, datasets, false, new int[]{1, 2}, new int[]{1, 2});
+            var study3 = new Study("Test-Study", 2, pods);
+
             Assert.True(study1.Equals(study2.ToStudy()));
             Assert.False(study1.Equals(study3));
         }
