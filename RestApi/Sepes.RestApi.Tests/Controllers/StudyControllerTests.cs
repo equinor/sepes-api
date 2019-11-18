8ec1e27060d98955f38f038e7113d51ec1bd9378
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-18 11:54:51 +0100

    Update StudyControllerTests.cs

--- RestApi/Sepes.RestApi.Tests/Controllers/StudyControllerTests.cs
+++ RestApi/Sepes.RestApi.Tests/Controllers/StudyControllerTests.cs
@@ -17,7 +17,7 @@ namespace Sepes.RestApi.Tests.Controller
         public async Task StudyCreate()
         {
             var databaseMock = new Mock<ISepesDb>();
-            var controller = new StudyController(databaseMock.Object);
+            var controller = new StudyController(databaseMock.Object, new Mock<IStudyService>().Object);
             databaseMock.Setup(db => db.createStudy("TestStudy", new int[] { 2, 3 }, new int[] { 2, 42 })).ReturnsAsync(42);
 
             Study study = new Study("test", 12, new List<Pod>(), new List<User>(), new List<User>(), new List<DataSet>(), false, new int[]{}, new int[]{});
@@ -31,7 +31,7 @@ namespace Sepes.RestApi.Tests.Controller
         public async Task StudyUpdate()
         {
             var databaseMock = new Mock<ISepesDb>();
-            var controller = new StudyController(databaseMock.Object);
+            var controller = new StudyController(databaseMock.Object, new Mock<IStudyService>().Object);
             databaseMock.Setup(db => db.updateStudy(42, false)).ReturnsAsync(1);
             Study study = new Study("test", 12, new List<Pod>(), new List<User>(), new List<User>(), new List<DataSet>(), false, new int[]{}, new int[]{});
             var result = await controller.UpdateVars(study);
@@ -42,7 +42,7 @@ namespace Sepes.RestApi.Tests.Controller
         public async Task StudyList()
         {
             var databaseMock = new Mock<ISepesDb>();
-            var controller = new StudyController(databaseMock.Object);
+            var controller = new StudyController(databaseMock.Object, new Mock<IStudyService>().Object);
             databaseMock.Setup(db => db.getStudies(false)).ReturnsAsync("TestData");
             var result = await controller.GetStudies();
 
@@ -52,7 +52,7 @@ namespace Sepes.RestApi.Tests.Controller
         public async Task GetArchived()
         {
             var databaseMock = new Mock<ISepesDb>();
-            var controller = new StudyController(databaseMock.Object);
+            var controller = new StudyController(databaseMock.Object, new Mock<IStudyService>().Object);
             databaseMock.Setup(db => db.getStudies(true)).ReturnsAsync("TestData");
             var result = await controller.GetArchivedStudies();
 
@@ -62,7 +62,7 @@ namespace Sepes.RestApi.Tests.Controller
         public async Task GetDataset()
         {
             var databaseMock = new Mock<ISepesDb>();
-            var controller = new StudyController(databaseMock.Object);
+            var controller = new StudyController(databaseMock.Object, new Mock<IStudyService>().Object);
             databaseMock.Setup(db => db.getDatasetList()).ReturnsAsync("DatasetList");
             var result = await controller.GetDataset();
             Assert.Equal("DatasetList", result);
