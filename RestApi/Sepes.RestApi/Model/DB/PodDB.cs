f5dfe569bf6510cf361b6e3ea1c639d37a3eb6fe
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-22 11:41:43 +0100

    Using UserDB models

--- RestApi/Sepes.RestApi/Model/DB/PodDB.cs
+++ RestApi/Sepes.RestApi/Model/DB/PodDB.cs
@@ -1,12 +1,8 @@
-using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Net;
 
 namespace Sepes.RestApi.Model
 {
-    //Internal version of pod model
     public class PodDB
     {
         public ushort id { get; set; }
@@ -15,16 +11,16 @@ namespace Sepes.RestApi.Model
         public bool allowAll { get; set; }
         public List<RuleDB> incoming { get; set; }
         public List<RuleDB> outgoing { get; set; }
-        //public List<User> users { get; set; }
-        //public ImmutableList<DataSet> locked2 { get; set; }
-        //public ImmutableList<DataSet> loaded2 { get; set; }
+        public List<UserDB> users { get; set; }
+        //public ImmutableList<DataSetDB> locked { get; set; }
+        //public ImmutableList<DataSetDB> loaded { get; set; }
 
 
         public Pod ToPod()
         {
             var podIncoming = incoming != null ? incoming.Select(r => r.ToRule()) : new List<Rule>();
             var podOutgoing = outgoing != null ? outgoing.Select(r => r.ToRule()) : new List<Rule>();
-            var podUsers = /*users != null ? users :*/ new List<User>();
+            var podUsers = users != null ? users.Select(u => u.ToUser()) : new List<User>();
 
             return new Pod(id, name, studyId, false, podIncoming, podOutgoing, podUsers, new List<DataSet>(), new List<DataSet>());
         }
