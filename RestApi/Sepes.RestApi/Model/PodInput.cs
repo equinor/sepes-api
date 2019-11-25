42cae35058c6c638dde063678b3c32fb6bee3686
Kristoffer <kristoffer.ardal@gmail.com>
2019-11-25 15:20:35 +0100

    Using default values

--- RestApi/Sepes.RestApi/Model/PodInput.cs
+++ RestApi/Sepes.RestApi/Model/PodInput.cs
@@ -9,16 +9,16 @@ namespace Sepes.RestApi.Model
         public string podName { get; set; }
         public int studyId { get; set; }
         public string tag { get; set; }
-        public RuleInput[] incoming { get; set; }
-        public RuleInput[] outgoing { get; set; }
-        public string[] users { get; set; }
+        public RuleInput[] incoming { get; set; } = new RuleInput[]{};
+        public RuleInput[] outgoing { get; set; } = new RuleInput[]{};
+        public string[] users { get; set; } = new string[]{};
 
 
         public Pod ToPod()
         {
-            var podIncoming = incoming != null ? incoming.Select(r => r.ToRule()) : new List<Rule>();
-            var podOutgoing = outgoing != null ? outgoing.Select(r => r.ToRule()) : new List<Rule>();
-            var podUsers = users != null ? users.Select(u => new User("", u, "")) : new List<User>();
+            var podIncoming = incoming.Select(r => r.ToRule());
+            var podOutgoing = outgoing.Select(r => r.ToRule());
+            var podUsers = users.Select(u => new User("", u, ""));
 
             return new Pod(podId, podName, studyId, false, podIncoming, podOutgoing, podUsers, new List<DataSet>(), new List<DataSet>());
         }
