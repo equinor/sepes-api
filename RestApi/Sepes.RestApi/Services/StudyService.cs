6823cba8b1974970fc350cf74522c4c03d9c23e0
Kris2ffer <kristoffer.ardal@gmail.com>
2019-11-27 16:53:34 +0100

    Added check for pods

--- RestApi/Sepes.RestApi/Services/StudyService.cs
+++ RestApi/Sepes.RestApi/Services/StudyService.cs
@@ -2,13 +2,11 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Sepes.RestApi.Model;
 
-using System.Text.Json;
 using System.Linq;
-using System;
 
 namespace Sepes.RestApi.Services {
 
-    // the main job of the Study Service is to own and keep track Study and Pod state.
+    // The main job of the Study Service is to own and keep track Study and Pod state.
     // This service is the only one that can change the internal representation of Study and pod.
     // It's also responsible of saving new changes. 
     public interface IStudyService
@@ -16,8 +14,8 @@ namespace Sepes.RestApi.Services {
         // Get the list of studies based on a user.
         IEnumerable<Study> GetStudies(User user, bool archived);
 
-        // makes changes to the meta data of a study.
-        // if based is null it means its a new study.
+        // Makes changes to the meta data of a study.
+        // If based is null it means its a new study.
         // This call will only succeed if based is the same as the current version of that study.
         // This method will only update the metadata about a study and will not make changes to the azure resources.
         // Use the PodServise for that.
@@ -46,23 +44,37 @@ namespace Sepes.RestApi.Services {
         public async Task<Study> Save(Study newStudy, Study based)
         {
             Study study = newStudy;
+
             if (based == null)
             {
                 study = await _db.NewStudy(newStudy);
+                _studies.Add(study);
             }
-
-            _studies.Add(study);
-
-            if (_studies.Contains(based))
+            else if (_studies.Contains(based))
             {
-                Console.WriteLine("####### Contains based study");
-
-                // Look for new pod
-                if (study.pods.Any(pod => !pod.id.HasValue))
+                foreach (var pod in study.pods)
                 {
-                    Pod pod = study.pods.ToList().Find(pod => !pod.id.HasValue);
-                    Pod newPod = pod.NewPodId(numberOfPods++);
+                    if (!based.pods.Contains(pod))
+                    {
+                        if (!pod.id.HasValue) // new pod
+                        {
+                            // generate new pod with id
+                            Pod newPod = pod.NewPodId(numberOfPods++);
+
+                            // _podService.Set(newPod, null);
+                            
+                            var newPods = study.pods.Remove(pod).Add(newPod);
+                            study = study.ReplacePods(newPods);
+                        }
+                        else // edited pod
+                        {
+                            Pod basePod = based.pods.ToList().Find(basePod => basePod.id == pod.id);
+                            // _podService.Set(pod, basePod);
+                        }
+                    }
                 }
+
+                _studies.Add(study);
             }
 
             return study;
@@ -71,7 +83,6 @@ namespace Sepes.RestApi.Services {
         public void LoadStudies()
         {
             _studies = _db.GetAllStudies().Result.ToHashSet();
-
             numberOfPods = (ushort) _studies.Sum(study => study.pods.Count);
         }
     }
