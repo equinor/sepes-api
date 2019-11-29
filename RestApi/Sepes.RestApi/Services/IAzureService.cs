524d813d0a5315b8ed9044687a7defd3ae929b6a
Kris2ffer <kristoffer.ardal@gmail.com>
2019-11-29 16:41:10 +0100

    Update IAzureService.cs

--- RestApi/Sepes.RestApi/Services/IAzureService.cs
+++ RestApi/Sepes.RestApi/Services/IAzureService.cs
@@ -11,6 +11,10 @@ namespace Sepes.RestApi.Services
         Task DeleteSecurityGroup(string securityGroupName, string resourceGroupName);
         Task ApplySecurityGroup(string resourceGroupName, string securityGroupName, string subnetName, string networkName);
         Task RemoveSecurityGroup(string resourceGroupName, string subnetName, string networkName);
+
+        Task NsgAllowOutboundPort(string securityGroupName, string resourceGroupName, string ruleName, int priority, string[] internalAddresses, int internalPort);
+        Task NsgAllowInboundPort(string securityGroupName, string resourceGroupName, string ruleName, int priority, string[] externalAddresses, int externalPort);
+        
         Task<string> AddUserToResourceGroup(string userId, string resourceGroupName);
         Task<string> AddUserToNetwork(string userId, string networkName);
     }
