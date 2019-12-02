617f5a6a820b0d2205d9773595f916f230184410
Kris2ffer <kristoffer.ardal@gmail.com>
2019-12-02 13:52:43 +0100

    Generate and return NSG name

--- RestApi/Sepes.RestApi/Services/IAzureService.cs
+++ RestApi/Sepes.RestApi/Services/IAzureService.cs
@@ -7,7 +7,7 @@ namespace Sepes.RestApi.Services
         Task<string> CreateResourceGroup(string podName);
         Task TerminateResourceGroup(string resourceGroupName);
         Task<string> CreateNetwork(string networkName, string addressSpace);
-        Task CreateSecurityGroup(string securityGroupName, string resourceGroupName);
+        Task<string> CreateSecurityGroup(string securityGroupName, string resourceGroupName);
         Task DeleteSecurityGroup(string securityGroupName, string resourceGroupName);
         Task ApplySecurityGroup(string resourceGroupName, string securityGroupName, string subnetName, string networkName);
         Task RemoveSecurityGroup(string resourceGroupName, string subnetName, string networkName);
