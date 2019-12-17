52ca5436cfb0a15cd3b1ae70ab4c3197dd981d14
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-12-17 13:27:44 +0100

    Replaced checkbox with switch

--- FrontEnd/src/components/PodPage.js
+++ FrontEnd/src/components/PodPage.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
 import PodRules from './PodRules';
 import PodDataset from './PodDataset.js';
@@ -6,6 +6,7 @@ import PodDataset from './PodDataset.js';
 import * as StudyService from "../studyService"
 
 import Sepes from '../sepes.js';
+import nsgswitch from './nsgSwitch';
 const sepes = new Sepes();
 
 class PodPage extends Component {
@@ -24,33 +25,31 @@ class PodPage extends Component {
     }
     render() {
         return (
-        <div>
-            <header>
-                <span><b>
-                    <span className="link" onClick={() => this.props.changePage("studies")}>Sepes</span> > <span 
-                        className="link" onClick={() => this.props.changePage("study")}>Study</span> > Pod </b></span>
+            <div>
+                <header>
+                    <span><b>
+                        <span className="link" onClick={() => this.props.changePage("studies")}>Sepes</span> > <span
+                            className="link" onClick={() => this.props.changePage("study")}>Study</span> > Pod </b></span>
                     <link />
-                <input type="text" placeholder="Pod name" id="new-study-input" value={this.state.podName} onChange={(e)=> this.setState({podName: e.target.value})} />
-                <button disabled={this.state.saveBtnDisabled} onClick={this.createPod}>Save</button>
-                <span className="loggedInUser">Logged in as <b>{ this.props.state.userName }</b></span>
-            </header>
-            <div className="sidebar podsidebar">
-                <div>
-                    <div style={{padding: "20px"}}>
-                        <label><input type="checkbox" onChange={this.openInternet()} />
-                            Open internet for this pod
-                        </label>
+                    <input type="text" placeholder="Pod name" id="new-study-input" value={this.state.podName} onChange={(e) => this.setState({ podName: e.target.value })} />
+                    <button disabled={this.state.saveBtnDisabled} onClick={this.createPod}>Save</button>
+                    <span className="loggedInUser">Logged in as <b>{this.props.state.userName}</b></span>
+                </header>
+                <div className="sidebar podsidebar">
+                    <div>
+                        <div style={{ padding: "20px" }}>
+                            {nsgswitch( this.state.openInternet, this.toggleNSG())}
+                        </div>
                     </div>
+                    <PodRules header="Incoming rules" data={this.state.incoming} addItem={this.addIncomingRule} removeItem={this.removeIncomingRule} />
+                    <PodRules header="Outgoing rules" data={this.state.outgoing} addItem={this.addOutgoingRule} removeItem={this.removeOutgoingRule} />
                 </div>
-                <PodRules header="Incoming rules" data={this.state.incoming} addItem={this.addIncomingRule} removeItem={this.removeIncomingRule}/>
-                <PodRules header="Outgoing rules" data={this.state.outgoing} addItem={this.addOutgoingRule} removeItem={this.removeOutgoingRule}/>
-            </div>
-            <div id="pod-dataset-list">
-                { this.props.state.selection.dataset.map((item) => (
-                    <PodDataset header={item} />
-                ))}
-            </div>
-        </div>);
+                <div id="pod-dataset-list">
+                    {this.props.state.selection.dataset.map((item) => (
+                        <PodDataset header={item} />
+                    ))}
+                </div>
+            </div>);
     }
 
     componentDidMount() {
@@ -63,13 +62,18 @@ class PodPage extends Component {
                 podId: pod.podId
             });
         }
-        
     }
-    openInternet(isOpen) {
-        this.setState({
-            //Open internet variabel.
-            //openInternet = isOpen
-        })
+    toggleNSG(input) {
+        if (input) {
+            alert("checked")
+            /*this.setState({
+                openInternet: true
+            });*/
+        } else {
+            /*this.setState({
+                openInternet: false
+            });*/
+        }
     }
 
     addIncomingRule = (port, ip) => {
@@ -83,7 +87,7 @@ class PodPage extends Component {
             incoming: StudyService.removeRule(rule.port, rule.ip, this.state.incoming)
         });
     }
-    
+
     addOutgoingRule = (port, ip) => {
         this.setState({
             outgoing: StudyService.addRule(port, ip, this.state.outgoing)
@@ -97,8 +101,8 @@ class PodPage extends Component {
     }
 
     createPod = () => {
-        this.setState({saveBtnDisabled: true});
-        
+        this.setState({ saveBtnDisabled: true });
+
         console.log(`New pod: ${this.props.state.selectedStudy.StudyId}, ${this.state.podName}`)
 
         let based = this.props.state.selectedStudy;
@@ -125,22 +129,22 @@ class PodPage extends Component {
         }
 
         console.log([study, based])
-        
+
         sepes.createStudy(study, based)
             .then(returnValue => returnValue.json())
             .then(study => {
                 if (this.state.podId === null) {
-                    let podId = study.pods[study.pods.length-1].podId
+                    let podId = study.pods[study.pods.length - 1].podId
                     this.setState({
                         podId
                     });
                     console.log(study);
                 }
                 this.props.setStudy(study);
-                this.setState({saveBtnDisabled: false});
+                this.setState({ saveBtnDisabled: false });
             })
             .catch(() => {
-                this.setState({saveBtnDisabled: false});
+                this.setState({ saveBtnDisabled: false });
             });
     }
 }
