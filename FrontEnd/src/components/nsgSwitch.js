d9907c9e1104dd85a375dacabe9dfe57724d7879
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2019-12-17 13:27:44 +0100

    Replaced checkbox with switch

---
+++ FrontEnd/src/components/nsgSwitch.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './nsgSwitch.css';
+
+const Switch = ({ isOn, handleToggle }) => {
+    return (
+      <>
+        <input
+          checked={isOn}
+          onChange={handleToggle}
+          className="react-switch-checkbox"
+          id={`react-switch-new`}
+          type="checkbox"
+        />
+        <label
+          className="react-switch-label"
+          htmlFor={`react-switch-new`}
+        >
+          <span className={`react-switch-button`} />
+        </label>
+      </>
+    );
+  };
+
+export default Switch;
