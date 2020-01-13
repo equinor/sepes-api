d2550ab2d2b7cd6184431cea1bd6fbe633ada43e
Unicus-Tom <51700565+Unicus-Tom@users.noreply.github.com>
2020-01-13 14:29:09 +0100

    Cleaned up DB service

--- RestApi/Sepes.RestApi/Services/ISepesDb.cs
+++ RestApi/Sepes.RestApi/Services/ISepesDb.cs
@@ -7,9 +7,6 @@ namespace Sepes.RestApi.Services
     public interface ISepesDb
     {
         //Task<string> getDatasetList();
-
-        Task<int> createStudy(string studyName, int[] userIds, int[] datasetIds);
-        Task<Pod> createPod(string name, int studyId);
         Task<string> getDatasetList();
         Task<Study> NewStudy(Study study);
         Task<bool> UpdateStudy(Study study);
