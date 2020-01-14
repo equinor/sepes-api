26a7acd794085e03b4a2e31437f027c25b4540c2
Kristoffer Årdal <kristoffer.ardal@gmail.com>
2020-01-14 10:29:24 +0100

    Removed old methods

--- RestApi/Sepes.RestApi.Tests/Mocks/PodMock.cs
+++ RestApi/Sepes.RestApi.Tests/Mocks/PodMock.cs
@@ -9,44 +9,7 @@ namespace Sepes.RestApi.Tests.Mocks
 {
     internal class PodMock : IPodService
     {
-        public Task applyNsg(string resourceGroupName, string securityGroupName, string subnetName, string networkName)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<Pod> CreateNewPod(string name, int userId)
-        {
-            return Task.FromResult(new Pod(42, name, userId, false, new List<Rule>(), new List<Rule>(), new List<User>(), new List<DataSet>(), new List<DataSet>()));
-        }
-        public Task createNsg(string securityGroupName, string resourceGroupName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task deleteNsg(string securityGroupName, string resourceGroupName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UInt16> deleteUnused()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task removeNsg(string resourceGroupName, string subnetName, string networkName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task switchNsg(string resourceGroupName, string securityGroupName, string subnetName, string networkName)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<string> GetPods(int studyID)
-        {
-            return Task.FromResult(JsonSerializer.Serialize(new Pod(42, "name", studyID, false, new List<Rule>(), new List<Rule>(), new List<User>(), new List<DataSet>(), new List<DataSet>())));
-        }
-
-        public Task Set(Pod newPod, Pod based)
+        public Task Set(Pod newPod, Pod based, IEnumerable<User> newUsers, IEnumerable<User> basedUsers)
         {
             throw new NotImplementedException();
         }
