aca9739ef254469b121b66887b3bb188bd05ebc6
Kristoffer Årdal <kris_aardal@hotmail.com>
2020-01-28 16:51:51 +0100

    Refactored delete functionality

--- RestApi/Sepes.RestApi/Services/StudyService.cs
+++ RestApi/Sepes.RestApi/Services/StudyService.cs
@@ -20,7 +20,6 @@ namespace Sepes.RestApi.Services
         // This call will only succeed if based is the same as the current version of that study.
         // This method will only update the metadata about a study and will not make changes to the azure resources.
         Task<Study> Save(Study newStudy, Study based);
-        Task<Study> DeletePod(Pod pod);
     }
 
     public class StudyService : IStudyService
@@ -56,7 +55,7 @@ namespace Sepes.RestApi.Services
             }
             else if (_studies.Contains(based))
             {
-                study = await UpdatePods(based, study);
+                study = await ManagePods(based, study);
                 await _db.UpdateStudy(study);
 
                 _studies.Remove(based);
@@ -66,44 +65,19 @@ namespace Sepes.RestApi.Services
             return study;
         }
 
-        public async Task<Study> DeletePod(Pod pod)
+        private async Task<Study> ManagePods(Study based, Study study)
         {
-            Study study = null;
+            study = await CreateOrUpdatePods(based, study);
 
-            foreach (var iStudy in _studies.ToList())
+            if (study.archived && based.archived)
             {
-                if (iStudy.studyId == pod.studyId)
-                {
-                    study = iStudy;
-                }
-
+                await DeletePods(based, study);
             }
-
-            if (study.pods.Contains(pod))
-            {
-                await _podService.Set(null, pod, null, null);
-                //Remove pod from study
-                var tempPods = study.pods.Remove(pod);
-                var studynew = study.ReplacePods(tempPods);
-                //Delete from database
-                await _db.UpdateStudy(studynew);
-                //Replace study in memory
-                _studies.Remove(study);
-                _studies.Add(studynew);
-                return studynew;
-
-            }
-            else
-            {
-                return null; //Error data mismatch, pod changed or not found. Refresh and try again
-            }
-
-            //If results to 1 it implies pod is in memory and matches.
-            //If 2 implies that pod is in memory but different state. Returns error to frontend telling user to refresh pod and reverify as it might be in active use
-            //If checking fails to find pod it returns 0 and triggers error to frontend telling it to ask user to refresh
+            
+            return study;
         }
 
-        private async Task<Study> UpdatePods(Study based, Study study)
+        private async Task<Study> CreateOrUpdatePods(Study based, Study study)
         {
             foreach (var pod in study.pods)
             {
@@ -130,6 +104,19 @@ namespace Sepes.RestApi.Services
             }
 
             return study;
+        }
+
+        private async Task DeletePods(Study based, Study study)
+        {
+            List<Task> deletePodTasks = new List<Task>();
+            foreach (var pod in based.pods)
+            {
+                if (!study.pods.Contains(pod))
+                {
+                    deletePodTasks.Add(_podService.Set(null, pod, null, null));
+                }
+            }
+            await Task.WhenAll(deletePodTasks.ToArray());
         }
 
         public void LoadStudies()
