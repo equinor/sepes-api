b0a5f0d6369a33ee3719a6fe22710ece81800f0b
Kristoffer Årdal <kris_aardal@hotmail.com>
2020-01-28 16:51:07 +0100

    Removed delete code

--- RestApi/Sepes.RestApi/Controllers/StudyController.cs
+++ RestApi/Sepes.RestApi/Controllers/StudyController.cs
@@ -31,62 +31,11 @@ namespace Sepes.RestApi.Controller
         {
             //Studies [1] is what the frontend claims the changes is based on while Studies [0] is the new version
             
-            
             //If based is null it can be assumed this will be a newly created study
             if (studies[1] == null)
             {
                 Study study = await _studyService.Save(studies[0].ToStudy(), null);
                 return study.ToStudyInput();
-            }
-            //If based has more pods than new version then it can be assumed pods are requested to be deleted.
-            else if (studies[1].pods.Count() > studies[0].pods.Count())
-            {
-                
-                List<ushort?> podIdBased = new List<ushort?>();
-                List<ushort?> podIdNew = new List<ushort?>();
-                
-                foreach (PodInput podBased in studies[1].pods)
-                {
-                    podIdBased.Add(podBased.podId);
-                }
-                foreach (PodInput podNew in studies[0].pods)
-                {
-                    podIdNew.Add(podNew.podId);
-                }
-
-                var podIdDiff = podIdBased.Except(podIdNew);
-
-                //Make sure only one deletion has been ordered
-                if (podIdDiff.Count() == 1)
-                {
-                    //Fetch the pod whose podId matches
-                    foreach (PodInput podIn in studies[1].pods)
-                    {
-                        if (podIn.podId == podIdDiff.First())
-                        {
-                            //Perform delete
-                            var response = await _studyService.DeletePod(podIn.ToPod());
-
-                            if (response != null)
-                            {
-                                return Ok(response);
-                            }
-                            else if (response == null)
-                            {
-                                return BadRequest("Error: Pod does not match memory or is not found. Refresh and try again.");
-                            }
-                            else
-                            {
-                                return StatusCode(500, "Deletion status unknown. Refresh browser and check again.");
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    return BadRequest("Too many deletion requests. Only single deletions supported at this time.");
-                }
-                return BadRequest("Unable to process request");
             }
             //Otherwise it must be a change.
             else
