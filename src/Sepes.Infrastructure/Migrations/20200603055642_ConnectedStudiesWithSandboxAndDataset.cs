c6050560c4d25552d003e2c2b45b5e1bacae9868
krsteen <kristoffer.steen@hotmail.com>
2020-06-03 08:08:18 +0200

    feat: connected Study with Sandbox and DataSet

---
+++ src/Sepes.Infrastructure/Migrations/20200603055642_ConnectedStudiesWithSandboxAndDataset.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sepes.Infrastructure.Migrations
+{
+    public partial class ConnectedStudiesWithSandboxAndDataset : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StudyId",
+                table: "SandBoxes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StudyId",
+                table: "DataSets",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SandBoxes_StudyId",
+                table: "SandBoxes",
+                column: "StudyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataSets_StudyId",
+                table: "DataSets",
+                column: "StudyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DataSets_Studies_StudyId",
+                table: "DataSets",
+                column: "StudyId",
+                principalTable: "Studies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SandBoxes_Studies_StudyId",
+                table: "SandBoxes",
+                column: "StudyId",
+                principalTable: "Studies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DataSets_Studies_StudyId",
+                table: "DataSets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SandBoxes_Studies_StudyId",
+                table: "SandBoxes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SandBoxes_StudyId",
+                table: "SandBoxes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DataSets_StudyId",
+                table: "DataSets");
+
+            migrationBuilder.DropColumn(
+                name: "StudyId",
+                table: "SandBoxes");
+
+            migrationBuilder.DropColumn(
+                name: "StudyId",
+                table: "DataSets");
+        }
+    }
+}
--- src/Sepes.Infrastructure/Model/Sandbox.cs
+++ src/Sepes.Infrastructure/Model/Sandbox.cs
@@ -4,6 +4,10 @@ namespace Sepes.Infrastructure.Model
 {
     public class SandBox : UpdateableBaseModel
     { 
-        public string Name { get; set; }      
+        public string Name { get; set; }
+
+        public Study Study { get; set; }
+
+        public int StudyId { get; set; }
     }
 }
