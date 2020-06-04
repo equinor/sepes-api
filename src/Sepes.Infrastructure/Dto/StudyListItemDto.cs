c8c18816f1941f211e09d0b8d96930e71227a02b
krsteen <kristoffer.steen@hotmail.com>
2020-06-04 11:21:09 +0200

    feat(errorhandlingandresponse): starting implementing error handling and response

--- src/Sepes.Infrastructure/Dto/StudyListItemDto.cs
+++ src/Sepes.Infrastructure/Dto/StudyListItemDto.cs
@@ -2,17 +2,12 @@
 
 namespace Sepes.Infrastructure.Dto
 {
-    public class StudyDto : UpdateableBaseDto
+    public class StudyListItemDto : UpdateableBaseDto
     {
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public string WbsCode { get; set; }
-
         public string LogoUrl { get; set; }
 
-        public List<DataSetDto> DataSets { get; set; }
-
-        public ICollection<SandBoxDto> SandBoxes { get; set; }
     }
 }
---
+++ src/Sepes.RestApi/Util/ApplicationInsightsUtil.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Linq;
+
+namespace Sepes.RestApi.Util
+{
+    public static class ApplicationInsightsUtil
+    {
+        public static string GetOperationId() {
+
+            var operationId = default(string);
+
+            try
+            {
+                var telemetry = new RequestTelemetry();
+
+                TelemetryConfiguration
+                    .Active
+                    .TelemetryInitializers
+                    .OfType<OperationCorrelationTelemetryInitializer>()
+                    .Single()
+                    .Initialize(telemetry);
+
+                operationId = telemetry.Context.Operation.Id;
+
+            }
+            catch {
+              
+            }
+
+            return operationId;
+        }      
+    }
+}
