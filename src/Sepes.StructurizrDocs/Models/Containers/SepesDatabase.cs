defabe7888662809d2c62b7cc37e8ca2d6497774
krsteen <kristoffer.steen@hotmail.com>
2020-06-11 13:06:16 +0200

    docs(added structurizr doc project): testing out structurizr documentation with PlantUml export

---
+++ src/Sepes.StructurizrDocs/Models/Containers/SepesDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sepes.StructurizrDocs.Models.Containers
+{
+    public class SepesDatabase
+    {
+    }
+}
---
+++ src/Sepes.StructurizrDocs/Models/SoftwareSystems/SepesSystemFactory.cs
@@ -0,0 +1,18 @@
+﻿using Structurizr;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sepes.StructurizrDocs.Models.SoftwareSystems.External
+{
+    public static class SepesSystemFactory
+    {
+        public static SoftwareSystem Sepes(Model model)
+        {
+            var sepesSystem = model.AddSoftwareSystem(Location.Internal, "Sepes", "Allows users to administer Studies and subcomponents.");
+           
+
+            return sepesSystem;
+        }
+    }
+}
