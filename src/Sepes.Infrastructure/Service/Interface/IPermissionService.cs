e922a3a009f8be79a5de3e348811735fe7a8438b
krsteen <kristoffer.steen@hotmail.com>
2020-06-18 13:38:59 +0200

    feat(studypermissions): starting work on permissions for studies

---
+++ src/Sepes.Infrastructure/Service/Interface/IPermissionService.cs
@@ -0,0 +1,21 @@
+﻿using Sepes.Infrastructure.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sepes.Infrastructure.Service.Interface
+{
+    public interface IPermissionService
+    {
+        Task StudyCreate();
+        Task StudyDelete();
+        Task StudyRestrict();
+        
+
+
+        Task<bool> CanDeleteStudy();
+
+
+
+
+    }
+}
