fcbe74d38c3b5d6294765f02f2f5e8d81242a8fe
Hans Kristian Vik Olsen <hanol@equinor.com>
2020-07-01 09:55:52 +0200

    fix(participant): added fields that were missing. Role is still missing

--- src/Sepes.Infrastructure/Dto/ParticipantDto.cs
+++ src/Sepes.Infrastructure/Dto/ParticipantDto.cs
@@ -5,7 +5,7 @@ namespace Sepes.Infrastructure.Dto
     public class ParticipantDto : UpdateableBaseDto
     {
         public string Name { get; set; }
-        public string Description { get; set; } 
-
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
     }
 }
