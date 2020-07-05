8a02862b9c350778fded6eab6c2fd31e50aa41ba
krsteen <kristoffer.steen@hotmail.com>
2020-07-05 21:23:36 +0200

    feat(sastokenforlogos): now generating sas token for logos

--- src/Sepes.Infrastructure/Service/AzureBlobStorageService.cs
+++ src/Sepes.Infrastructure/Service/AzureBlobStorageService.cs
@@ -1,21 +1,22 @@
-﻿using Azure.Storage.Blobs;
+﻿
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Sas;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Sepes.Infrastructure.Dto;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Azure.Storage;
-using System.Runtime.CompilerServices;
-using System.Linq;
 using System.IO;
-using Azure.Storage.Blobs.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Sepes.Infrastructure.Service
 {
-    class AzureBlobStorageService
+    public class AzureBlobStorageService : IAzureBlobStorageService
     {
-        private readonly string connectionString;
-        private readonly string containerName = "studylogos";
+        readonly string _connectionString;
+        public readonly string _containerName = "studylogos";
 
         public enum ImageFormat
         {
@@ -25,18 +26,30 @@ namespace Sepes.Infrastructure.Service
             png
         }
 
-        public AzureBlobStorageService(string connectionString)
+        public AzureBlobStorageService(IConfiguration config)
         {
-            this.connectionString = connectionString;
+            this._connectionString = config["AzureStorageConnectionString"];
         }
+
+        BlobContainerClient CreateBlobContainerClient()
+        {
+            return new BlobContainerClient(this._connectionString, _containerName);
+        }
+
+        BlobServiceClient CreateBlobServiceClient()
+        {
+            return new BlobServiceClient(this._connectionString);
+        }
+
         public string UploadBlob(IFormFile blob)
         {
             if (!FileIsCorrectImageFormat(blob))
             {
                 throw new ArgumentException("Blob has invalid filename or is not of type png, jpg or bmp.");
             }
+
             string suppliedFileName = blob.FileName;
-            BlobContainerClient blobContainerClient = new BlobContainerClient(this.connectionString, containerName);
+            var blobContainerClient = CreateBlobContainerClient();
             string fileName = Guid.NewGuid().ToString("N") + suppliedFileName;
             var fileStream = blob.OpenReadStream();
             blobContainerClient.UploadBlob(fileName, fileStream);
@@ -46,27 +59,83 @@ namespace Sepes.Infrastructure.Service
 
         public Azure.Response<bool> DeleteBlob(string fileName)
         {
-            BlobContainerClient blobContainerClient = new BlobContainerClient(this.connectionString, containerName);
+            var blobContainerClient = CreateBlobContainerClient();
             return blobContainerClient.DeleteBlobIfExists(fileName);
         }
 
-        public async System.Threading.Tasks.Task<byte[]> GetImageFromBlobAsync(string logoUrl)
+        public async Task<byte[]> GetImageFromBlobAsync(string logoUrl)
         {
-            BlobContainerClient blobContainerClient = new BlobContainerClient(this.connectionString, containerName);
-            var blobClient = blobContainerClient.GetBlockBlobClient(logoUrl);
-            MemoryStream memStream = new MemoryStream();
-            await blobClient.DownloadToAsync(memStream);
+            var blobContainerClient = CreateBlobContainerClient();
+            var blockBlobClient = blobContainerClient.GetBlockBlobClient(logoUrl);
+            var memStream = new MemoryStream();
+            await blockBlobClient.DownloadToAsync(memStream);
             return memStream.ToArray();
+        }
+
+        public async Task<IEnumerable<StudyListItemDto>> DecorateLogoUrlsWithSAS(IEnumerable<StudyListItemDto> studyDtos)
+        {
+            var uriBuilder = await CreateUriBuilderWithSasToken();
+
+            foreach (var curDto in studyDtos)
+            {
+                uriBuilder.Path = string.Format("{0}/{1}", _containerName, curDto.LogoUrl);
+                curDto.LogoUrl = uriBuilder.Uri.ToString();
+            }
+
+            return studyDtos;
+
+        }
+
+
+        public async Task<UriBuilder> CreateUriBuilderWithSasToken()
+        {
+            bool isDevelopmentStorage = this._connectionString == "UseDevelopmentStorage=true";
+
+            var blobServiceClient = new BlobServiceClient(this._connectionString);
+
+            var uriBuilder = new UriBuilder()
+            {
+                Scheme = "https",
+                Host = string.Format("{0}.blob.core.windows.net", blobServiceClient.AccountName),
+
+            };
+
+            if (isDevelopmentStorage)
+            {
+                return uriBuilder;
+            }
+            else
+            {
+
+                var delegationKey = await blobServiceClient.GetUserDelegationKeyAsync(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddDays(7));
+
+                var sasBuilder = new BlobSasBuilder()
+                {
+                    BlobContainerName = _containerName,
+                    Resource = "c",
+                    StartsOn = DateTimeOffset.UtcNow,
+                    ExpiresOn = DateTimeOffset.UtcNow.AddSeconds(30)
+                };
+
+                sasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+                var sasQueryParams = sasBuilder.ToSasQueryParameters(delegationKey, blobServiceClient.AccountName).ToString();
+
+                uriBuilder.Query = sasQueryParams;
+
+                return uriBuilder;
+            }
+
         }
 
         bool FileIsCorrectImageFormat(IFormFile file)
         {
             string suppliedFileName = file.FileName;
             string fileType = suppliedFileName.Split('.').Last();
-            if (!String.IsNullOrWhiteSpace(fileType) 
-                && (fileType.Equals(ImageFormat.png.ToString()) 
-                || fileType.Equals(ImageFormat.jpeg.ToString()) 
-                || fileType.Equals(ImageFormat.jpg.ToString()) 
+            if (!String.IsNullOrWhiteSpace(fileType)
+                && (fileType.Equals(ImageFormat.png.ToString())
+                || fileType.Equals(ImageFormat.jpeg.ToString())
+                || fileType.Equals(ImageFormat.jpg.ToString())
                 || fileType.Equals(ImageFormat.bmp.ToString()))
                 )
             {
