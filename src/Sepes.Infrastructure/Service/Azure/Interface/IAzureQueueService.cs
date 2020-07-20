838b9946e893064b4ced54b2b79d36e92d29d7e5
Kåre Obrestad <kaobr@equinor.com>
2020-07-20 10:09:52 +0200

    docs(azurequeueservice, iazurequeueservice): commenting

--- src/Sepes.Infrastructure/Service/Azure/Interface/IAzureQueueService.cs
+++ src/Sepes.Infrastructure/Service/Azure/Interface/IAzureQueueService.cs
@@ -10,11 +10,25 @@ namespace Sepes.Infrastructure.Service.Azure.Interface
     public interface IAzureQueueService
     {
         Task SendMessage(string message);
+
+        // Gets first message without removing from queue, but makes it invisible for 30 seconds.
         Task<QueueMessage> RecieveMessage();
+
+        // Gets message without removing from queue, but makes it invisible for 30 seconds.
         Task<IEnumerable<QueueMessage>> RecieveMessages(int numberOfMessages);
+
+        // Updates the message in-place in the queue.
+        // The message parameter is a message that has been fetched with RecieveMessage() or RecieveMessages()
         Task UpdateMessage(QueueMessage message, string updatedMessage, int timespan = 30);
+
+        // Message needs to be retrieved with recieveMessage() to be able to be deleted.
         Task DeleteMessage(QueueMessage message);
+
+        // Gets messages from queue without making them invisible.
         Task<IEnumerable<PeekedMessage>> PeekMessages(int numberOfMessages);
+
+        // Returns approximate number of messages in queue.
+        // The number is not lower than the actual number of messages in the queue, but could be higher.
         Task<int> GetNumberOfMessengesInQueue();
     }
 }
