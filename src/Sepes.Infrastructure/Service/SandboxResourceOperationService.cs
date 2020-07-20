18863e707857005cf20a9fa9e6c636d93a71d8c2
krsteen <kristoffer.steen@hotmail.com>
2020-07-20 15:08:39 +0200

    feat(deployazureresources): implemented SandboxResourceOperationService with basic methods

--- src/Sepes.Infrastructure/Service/SandboxResourceOperationService.cs
+++ src/Sepes.Infrastructure/Service/SandboxResourceOperationService.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Sepes.Infrastructure.Dto;
+using Sepes.Infrastructure.Exceptions;
+using Sepes.Infrastructure.Model;
 using Sepes.Infrastructure.Model.Context;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace Sepes.Infrastructure.Service
 {
@@ -15,6 +17,41 @@ namespace Sepes.Infrastructure.Service
         {
             _db = db;
             _mapper = mapper;
+        }
+
+        public async Task Add(SandboxResourceOperationDto operationDto)
+        {
+            var newOperation = _mapper.Map<SandboxResourceOperation>(operationDto);
+            _db.SandboxResourceOperations.Add(newOperation);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<SandboxResourceOperationDto> GetByIdAsync(int id)
+        {
+            var itemFromDb = await GetOrThrowAsync(id);
+            var itemDto = _mapper.Map<SandboxResourceOperationDto>(itemFromDb);
+            return itemDto;
+        }     
+
+        async Task<SandboxResourceOperation> GetOrThrowAsync(int id)
+        {
+            var entityFromDb = await _db.SandboxResourceOperations.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (entityFromDb == null)
+            {
+                throw NotFoundException.CreateForIdentity("SandboxResourceOperation", id);
+            }
+
+            return entityFromDb;
+        }
+
+        public async Task<SandboxResourceOperationDto> UpdateStatus(int id, string status)
+        {
+            var itemFromDb = await GetOrThrowAsync(id);
+            itemFromDb.Status = status;
+            await _db.SaveChangesAsync();
+
+            return await GetByIdAsync(itemFromDb.Id);
         }
 
 
