8adb2bdd1ebd130e4d1c253bd612ebd4a18ec635
Kåre Obrestad <kaobr@equinor.com>
2020-07-20 15:53:32 +0200

    feat(azurequeueservice + tests): made test class for QueueService, and improved functionality

--- src/Sepes.Infrastructure/Service/Azure/AzureQueueService.cs
+++ src/Sepes.Infrastructure/Service/Azure/AzureQueueService.cs
@@ -1,22 +1,29 @@
 ﻿using System; // Namespace for Console output
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks; // Namespace for Task
 using Azure.Storage.Queues; // Namespace for Queue storage types
 using Azure.Storage.Queues.Models; // Namespace for PeekedMessage
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Sepes.Infrastructure.Dto;
 using Sepes.Infrastructure.Service.Azure.Interface;
 
 namespace Sepes.Infrastructure.Service.Azure
 {
-    public class AzureQueueService : AzureServiceBase, IAzureQueueService
+    public class AzureQueueService : IAzureQueueService
     {
-        private const string _queueName = "sandbox-resource-operations";
+        private string _queueName = "sandbox-resource-operations";
         private readonly string _connectionString;
+        protected readonly IConfiguration _config;
+        protected readonly ILogger _logger;
 
-        public AzureQueueService(IConfiguration config, ILogger logger) : base(config, logger)
+        public AzureQueueService(IConfiguration config, ILogger logger)
         {
-            this._connectionString = config["AzureQueueConnectionString"];
+            _config = config;
+            _connectionString = config["AzureQueueConnectionString"];
+            _logger = logger;
         }
 
         // Helper method for creating queueClient
@@ -41,7 +48,7 @@ namespace Sepes.Infrastructure.Service.Azure
             return queueClient;
         }
 
-        // Message needs to be retrieved with recieveMessage() to be able to be deleted.
+        // Message needs to be retrieved with recieveMessage(s)() to be able to be deleted.
         public async Task DeleteMessage(QueueMessage message)
         {
             var client = await CreateQueueClient();
@@ -50,7 +57,7 @@ namespace Sepes.Infrastructure.Service.Azure
 
         // Returns approximate number of messages in queue.
         // The number is not lower than the actual number of messages in the queue, but could be higher.
-        public async Task<int> GetNumberOfMessengesInQueue()
+        public async Task<int> GetApproximateNumberOfMessengesInQueue()
         {
             var client = await CreateQueueClient();
             QueueProperties props = await client.GetPropertiesAsync();
@@ -58,6 +65,7 @@ namespace Sepes.Infrastructure.Service.Azure
         }
 
         // Gets messages from queue without making them invisible.
+        // Parameter numberOfMessages has a max of 32.
         public async Task<IEnumerable<PeekedMessage>> PeekMessages(int numberOfMessages)
         {
             var client = await CreateQueueClient();
@@ -65,21 +73,22 @@ namespace Sepes.Infrastructure.Service.Azure
             return peekedMessages;
         }
 
-        public async Task SendMessage(string message)
+        public async Task SendMessage(SandboxResourceOperationDto operationDto)
         {
             var client = await CreateQueueClient();
-            _ = await client.SendMessageAsync(message);
+            _ = await client.SendMessageAsync(SandboxResourceOperationToMessageString(operationDto));
         }
 
-        // Gets first message without removing from queue, but makes it invisible for 30 seconds.
+        // Gets first message as QueueMessage without removing from queue, but makes it invisible for 30 seconds.
         public async Task<QueueMessage> RecieveMessage()
         {
             var client = await CreateQueueClient();
             QueueMessage[] messages = await client.ReceiveMessagesAsync();
-            return messages[0];           
+            return messages.First();
         }
 
         // Gets message without removing from queue, but makes it invisible for 30 seconds.
+        // Parameter numberOfMessages has a max of 32.
         public async Task<IEnumerable<QueueMessage>> RecieveMessages(int numberOfMessages)
         {
             var client = await CreateQueueClient();
@@ -88,11 +97,41 @@ namespace Sepes.Infrastructure.Service.Azure
         }
 
         // Updates the message in-place in the queue.
-        // The message parameter is a message that has been fetched with RecieveMessage() or RecieveMessages()
+        // The message parameter is a message that has been fetched with RecieveMessageRaw() or RecieveMessages()
         public async Task UpdateMessage(QueueMessage message, string updatedMessage, int timespan = 30)
         {
             var client = await CreateQueueClient();
             _ = await client.UpdateMessageAsync(message.MessageId, message.PopReceipt, updatedMessage, TimeSpan.FromSeconds(timespan));
+        }
+    
+        public void UseTestingQueue()
+        {
+            _queueName = "unit-test-sandbox-resource-operations-queue";
+        }
+
+        public string SandboxResourceOperationToMessageString(SandboxResourceOperationDto operation)
+        {
+            return JsonConvert.SerializeObject(operation);
+        }
+
+        public SandboxResourceOperationDto MessageToSandboxResourceOperation(QueueMessage message)
+        {
+            return JsonConvert.DeserializeObject<SandboxResourceOperationDto>(message.MessageText);
+        }
+
+        public async Task DeleteQueue()
+        {
+            var client = await CreateQueueClient();
+            _ = await client.DeleteIfExistsAsync();
+
+        }
+
+        public async Task DeleteQueue(string queueName)
+        {
+            // Instantiate a QueueClient which will be used to create and manipulate the queue
+            QueueClient queueClient = new QueueClient(_connectionString, queueName);
+            // Create the queue if it doesn't already exist
+            _ = await queueClient.DeleteIfExistsAsync();
         }
     }
 }
--- src/Sepes.Infrastructure/Service/Azure/Interface/IAzureQueueService.cs
+++ src/Sepes.Infrastructure/Service/Azure/Interface/IAzureQueueService.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks; // Namespace for Task
 using Azure.Storage.Queues; // Namespace for Queue storage types
 using Azure.Storage.Queues.Models; // Namespace for PeekedMessage
+using Sepes.Infrastructure.Dto;
+using Newtonsoft.Json;
 
 namespace Sepes.Infrastructure.Service.Azure.Interface
 {
     public interface IAzureQueueService
     {
-        Task SendMessage(string message);
+        Task SendMessage(SandboxResourceOperationDto operationDto);
 
         // Gets first message without removing from queue, but makes it invisible for 30 seconds.
         Task<QueueMessage> RecieveMessage();
@@ -19,7 +22,7 @@ namespace Sepes.Infrastructure.Service.Azure.Interface
         // The message parameter is a message that has been fetched with RecieveMessage() or RecieveMessages()
         Task UpdateMessage(QueueMessage message, string updatedMessage, int timespan = 30);
 
-        // Message needs to be retrieved with recieveMessage() to be able to be deleted.
+        // Message needs to be retrieved with recieveMessage(s)() to be able to be deleted.
         Task DeleteMessage(QueueMessage message);
 
         // Gets messages from queue without making them invisible.
@@ -27,6 +30,17 @@ namespace Sepes.Infrastructure.Service.Azure.Interface
 
         // Returns approximate number of messages in queue.
         // The number is not lower than the actual number of messages in the queue, but could be higher.
-        Task<int> GetNumberOfMessengesInQueue();
+        Task<int> GetApproximateNumberOfMessengesInQueue();
+
+        // Method to allow Unit tests to use queue without interfering with production queue.
+        void UseTestingQueue();
+
+        string SandboxResourceOperationToMessageString(SandboxResourceOperationDto operation);
+
+        SandboxResourceOperationDto MessageToSandboxResourceOperation(QueueMessage message);
+
+        Task DeleteQueue();
+
+        Task DeleteQueue(string queueName);
     }
 }
