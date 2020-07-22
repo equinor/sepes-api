da62255b145414fa947b02cf02f433ecae591b85
Kåre Obrestad <kaobr@equinor.com>
2020-07-22 11:16:26 +0200

    feat(azureresourcenameutils): add methods for cleaning sandboxName. Also made sure stgAcc are unique
    Appends short guid to end of storageAccountNames. Methods added for cleaning whitespace from strings
    or making string alphanumeric.

--- src/Sepes.Infrastructure/Util/AzureResourceNameUtil.cs
+++ src/Sepes.Infrastructure/Util/AzureResourceNameUtil.cs
@@ -6,67 +6,68 @@ namespace Sepes.Infrastructure.Util
     // Naming prefixes should follow these conventions https://docs.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/naming-and-tagging.
     public static class AzureResourceNameUtil
     {
-        public static string ResourceGroupForStudy(string sandboxName)
+        public static string ResourceGroup(string sandboxName)
         {
-            return $"rg-study-{sandboxName}";
+            return StripWhitespace($"rg-study-{sandboxName}");
         }
         public static string Sandbox(string studyName)
         {
-            return $"{studyName}-sandbox";
+            return StripWhitespace($"{studyName}-sandbox");
         }
 
         public static string VNet(string studyName, string sandboxName)
         {
-            return $"vnet-study-{sandboxName}";
+            return StripWhitespace($"vnet-study-{sandboxName}");
         }
 
         public static string SubNet(string sandboxName)
         {
-            return $"snet-{sandboxName}";
+            return StripWhitespace($"snet-{sandboxName}");
         }
 
         public static string NetworkSecGroup(string sandboxName)
         {
-            return $"nsg-{sandboxName}";
+            return StripWhitespace($"nsg-{sandboxName}");
         }
 
         public static string NetworkSecGroupSubnet(string sandboxName)
         {
-            return $"nsg-snet-{sandboxName}";
+            return StripWhitespace($"nsg-snet-{sandboxName}");
         }
 
         public static string Bastion(string sandboxName)
         {
-            return $"bastion-{sandboxName}";
+            return StripWhitespace($"bastion-{sandboxName}");
         }
 
         public static string BastionPublicIp(string sandboxName)
         {
-            return $"pip-{sandboxName}-bastion";
+            return StripWhitespace($"pip-{sandboxName}-bastion");
         }
 
         // Storage account names must be between 3 and 24 characters in length and may contain numbers and lowercase letters only.
-        // Your storage account name must be unique within Azure.No two storage accounts can have the same name.
-        // Current solution might possibly allow for duplicate names.
+        // Your storage account name must be unique within Azure. No two storage accounts can have the same name.
         // StorageAccount names needs to be unique in Azure scope.
         public static string StorageAccount(string sandboxName)
         {
+            var shortGuid = Guid.NewGuid().ToString().ToLower().Substring(0, 3);
             sandboxName = MakeStringAlphanumeric(sandboxName);
-            if (sandboxName.Length > 21)
+            if (sandboxName.Length > 18)
             {
-                return $"st{sandboxName.ToLower().Substring(0, 21)}";
+                return $"st{sandboxName.ToLower().Substring(0, 18)}{shortGuid}";
             }
-            return $"st{sandboxName.ToLower()}";
+            return $"st{sandboxName.ToLower()}{shortGuid}";
         }
 
         public static string DiagnosticsStorageAccount(string sandboxName)
         {
+            var shortGuid = Guid.NewGuid().ToString().ToLower().Substring(0, 3);
             sandboxName = MakeStringAlphanumeric(sandboxName);
-            if (sandboxName.Length > 17)
+            if (sandboxName.Length > 14)
             {
-                return $"stdiag{sandboxName.ToLower().Substring(0, 17)}";
+                return $"stdiag{sandboxName.ToLower().Substring(0, 14)}{shortGuid}";
             }
-            return $"stdiag{sandboxName.ToLower()}";
+            return $"stdiag{sandboxName.ToLower()}{shortGuid}";
         }
 
         public static string VirtualMachine(string sandboxName)
@@ -77,8 +78,16 @@ namespace Sepes.Infrastructure.Util
         static string MakeStringAlphanumeric(string str)
         {
             return new string((from c in str
-                              where char.IsWhiteSpace(c) || char.IsLetterOrDigit(c)
+                              where char.IsLetterOrDigit(c) && !char.IsWhiteSpace(c)
                               select c
+                              ).ToArray());
+        }
+
+        static string StripWhitespace(string str)
+        {
+            return new string((from c in str
+                               where !char.IsWhiteSpace(c)
+                               select c
                               ).ToArray());
         }
     }
