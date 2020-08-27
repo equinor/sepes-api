3a6b08c98b49221731aa52b7bfa6871f1f9a6c85
krsteen <kristoffer.steen@hotmail.com>
2020-08-27 12:45:35 +0200

    feat(adduserservice): added a user service instead of the claims principal service
    Has more functionality than before. Also added a SepesUser class

--- src/Sepes.Infrastructure/Constants/Roles.cs
+++ src/Sepes.Infrastructure/Constants/Roles.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Sepes.Infrastructure.Constants
+﻿namespace Sepes.Infrastructure.Constants
 {
     public static class Roles
     {
+        //AD Roles
         public const string Admin = "Sepes-Admin";
-        public const string Sponsor = "Sepes-Sponsor";       
+        public const string Sponsor = "Sepes-Sponsor";
+        public const string DatasetAdmin = "Sepes-Dataset-Admin";
+
+        //Study specific roles, maintained in Sepes application
+        public const string SponsorRep = "Sepes-Sponsor-Rep";
+        public const string VendorAdmin= "Sepes-Vendor-Admin";
+        public const string VendorContributor = "Sepes-Vendor-Contributor";
+        public const string StudyViewer = "Sepes-Study-Viewer";       
     }
 
     public static class RoleSets
