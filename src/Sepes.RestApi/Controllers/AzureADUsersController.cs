a8cfaff3e33acbb1cb3d5685b12c31b2f86ed447
Hans Kristian Vik Olsen <hanol@equinor.com>
2020-09-09 13:34:14 +0200

    feat(ad azure user search): added endpoint to get users by searchstring

---
+++ src/Sepes.RestApi/Controllers/AzureADUsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using Sepes.Infrastructure.Dto;
+using Sepes.Infrastructure.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sepes.RestApi.Controllers
+{
+    [Route("azureadusers")]
+    [Authorize]
+    public class AzureADUserController : ControllerBase
+    {
+        private readonly IAzureADUsersService _azureADUsersService;
+
+        public AzureADUserController(IAzureADUsersService azureADUsersService)
+        {
+            _azureADUsersService = azureADUsersService;
+        }
+
+        [HttpGet("searchusers")]
+        [ProducesResponseType(typeof(AzureADUserDto), StatusCodes.Status200OK)]
+        [AuthorizeForScopes(Scopes = new[] { "User.Read.All" })]
+        public async Task<IActionResult> SearchUsers(string search, int limit)
+        {
+            var result = await _azureADUsersService.SearchUsers(search, limit);
+            return Ok(result);
+        }
+
+
+    }
+}
---
+++ src/Sepes.RestApi/GraphServiceProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using Sepes.Infrastructure.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sepes.RestApi
+{
+    public class GraphServiceProvider : IGraphServiceProvider
+    {
+        readonly ITokenAcquisition tokenAcquisition;
+
+        public GraphServiceProvider(ITokenAcquisition tokenAcquisition)
+        {
+            this.tokenAcquisition = tokenAcquisition;
+        }
+        public GraphServiceClient GetGraphServiceClient(string[] scopes)
+        {
+            return GraphServiceClientFactory.GetAuthenticatedGraphClient(async () =>
+            {
+                string result = await tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+                return result;
+            }, "https://graph.microsoft.com/v1.0/");
+        }
+    }
+}
