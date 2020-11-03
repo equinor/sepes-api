10343ef9ad8237ed8fa57b167bcbca2653ada9ba
Kristoffer Steen <kristoffer.steen@bouvet.no>
2020-11-03 12:17:54 +0100

    fix(azurequeueservicetest): was not building, commented out (#370)

--- src/Sepes.Tests/Services/AzureQueueServiceTests.cs
+++ src/Sepes.Tests/Services/AzureQueueServiceTests.cs
@@ -1,80 +1,80 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Sepes.Infrastructure.Service;
-using Sepes.Tests.Setup;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
+﻿//using Microsoft.Extensions.DependencyInjection;
+//using Sepes.Infrastructure.Service;
+//using Sepes.Tests.Setup;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Xunit;
 
 
-namespace Sepes.Tests.Services.Azure
-{
-    public class AzureQueueServiceTests
-    {
-        public ServiceCollection Services { get; private set; }
-        public ServiceProvider ServiceProvider { get; protected set; }        
+//namespace Sepes.Tests.Services.Azure
+//{
+//    public class AzureQueueServiceTests
+//    {
+//        public ServiceCollection Services { get; private set; }
+//        public ServiceProvider ServiceProvider { get; protected set; }        
 
-        public AzureQueueServiceTests()
-        {
-            Services = BasicServiceCollectionFactory.GetServiceCollectionWithInMemory();
-            Services.AddTransient<IAzureQueueService, AzureQueueService>();
-            ServiceProvider = Services.BuildServiceProvider();
-        }     
+//        public AzureQueueServiceTests()
+//        {
+//            Services = BasicServiceCollectionFactory.GetServiceCollectionWithInMemory();
+//            Services.AddTransient<IAzureQueueService, AzureQueueService>();
+//            ServiceProvider = Services.BuildServiceProvider();
+//        }     
 
-        IAzureQueueService Init()
-        {
-            var queueService = ServiceProvider.GetService<IAzureQueueService>();
-            return queueService;
-        }
+//        IAzureQueueService Init()
+//        {
+//            var queueService = ServiceProvider.GetService<IAzureQueueService>();
+//            return queueService;
+//        }
 
-        async Task Cleanup()
-        {
-            var queueService = ServiceProvider.GetService<IAzureQueueService>();
-            await queueService.DeleteQueueAsync();
-        }
+//        async Task Cleanup()
+//        {
+//            var queueService = ServiceProvider.GetService<IAzureQueueService>();
+//            await queueService.DeleteQueueAsync();
+//        }
 
-        //[Fact]
-        //public async void SendMessage_ShouldSendMessage()
-        //{
-        //    var queueService = Init();
+//        //[Fact]
+//        //public async void SendMessage_ShouldSendMessage()
+//        //{
+//        //    var queueService = Init();
           
-        //    var msgsBefore = await queueService.PeekMessagesAsync(32);
-        //    int noBefore = msgsBefore.ToList().Count;
+//        //    var msgsBefore = await queueService.PeekMessagesAsync(32);
+//        //    int noBefore = msgsBefore.ToList().Count;
 
-        //    await queueService.SendMessageAsync("Test message");
+//        //    await queueService.SendMessageAsync("Test message");
 
-        //    var msgsAfter = await queueService.PeekMessagesAsync(32);
-        //    int noAfter = msgsAfter.ToList().Count;
+//        //    var msgsAfter = await queueService.PeekMessagesAsync(32);
+//        //    int noAfter = msgsAfter.ToList().Count;
 
-        //    Assert.Equal<int>(noBefore + 1, noAfter);
-        //    await Cleanup();
-        //}
+//        //    Assert.Equal<int>(noBefore + 1, noAfter);
+//        //    await Cleanup();
+//        //}
 
-        //[Fact]
-        //public async void SandboxResourceOperationToMessageString_ShouldConvertToSameFormatAsFoundInQueue()
-        //{
-        //    var queueService = Init();
-        //    var convertedOperation = queueService.SandboxResourceOperationToMessageString(mockOperation);
-        //    await queueService.SendMessage(createMockMessage());
+//        //[Fact]
+//        //public async void SandboxResourceOperationToMessageString_ShouldConvertToSameFormatAsFoundInQueue()
+//        //{
+//        //    var queueService = Init();
+//        //    var convertedOperation = queueService.SandboxResourceOperationToMessageString(mockOperation);
+//        //    await queueService.SendMessage(createMockMessage());
 
-        //    var recievedMessageResult = await queueService.RecieveMessage();
+//        //    var recievedMessageResult = await queueService.RecieveMessage();
 
-        //    Assert.Equal(recievedMessageResult.MessageText, convertedOperation);
-        //    await Cleanup();
-        //}
+//        //    Assert.Equal(recievedMessageResult.MessageText, convertedOperation);
+//        //    await Cleanup();
+//        //}
 
-        //[Fact]
-        //public async void MessageToSandboxResourceOperation_ShouldConvertToCorrectObject()
-        //{
-        //    var queueService = Init();
-        //    await queueService.SendMessage(createMockMessage());
+//        //[Fact]
+//        //public async void MessageToSandboxResourceOperation_ShouldConvertToCorrectObject()
+//        //{
+//        //    var queueService = Init();
+//        //    await queueService.SendMessage(createMockMessage());
 
-        //    var recievedMessageResult = await queueService.RecieveMessages(5);
-        //    var recievedMessage = recievedMessageResult.First();
-        //    var deserializedMessage = queueService.MessageToSandboxResourceOperation(recievedMessage);
+//        //    var recievedMessageResult = await queueService.RecieveMessages(5);
+//        //    var recievedMessage = recievedMessageResult.First();
+//        //    var deserializedMessage = queueService.MessageToSandboxResourceOperation(recievedMessage);
 
-        //    Assert.Equal<SandboxResourceOperationDto>(mockOperation, deserializedMessage);
+//        //    Assert.Equal<SandboxResourceOperationDto>(mockOperation, deserializedMessage);
 
-        //    await Cleanup();
-        //}
-    }
-}
+//        //    await Cleanup();
+//        //}
+//    }
+//}
