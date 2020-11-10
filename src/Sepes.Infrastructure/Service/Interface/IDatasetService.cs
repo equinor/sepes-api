93b68284f899cbf0dc8712857d0f80b4970772eb
Hans Kristian Vik Olsen <hanol@equinor.com>
2020-11-10 10:22:36 +0100

    fix: changed IDatasetService getDatasetssForstudy to IEnumerable

--- src/Sepes.Infrastructure/Service/Interface/IDatasetService.cs
+++ src/Sepes.Infrastructure/Service/Interface/IDatasetService.cs
@@ -11,7 +11,7 @@ namespace Sepes.Infrastructure.Service.Interface
         Task<IEnumerable<DatasetDto>> GetDatasetsAsync();
         Task<DatasetDto> GetDatasetByDatasetIdAsync(int datasetId);
 
-        Task<List<DataSetsForStudyDto>> GetDatasetsForStudy(int studyId);
+        Task<IEnumerable<DataSetsForStudyDto>> GetDatasetsForStudy(int studyId);
         Task<DataSetsForStudyDto> GetDatasetByStudyIdAndDatasetIdAsync(int studyId, int datasetId);
         Task<DatasetDto> CreateDatasetAsync(DatasetDto newDataset);
         Task<DatasetDto> UpdateDatasetAsync(int datasetId, DatasetDto newDataset);
