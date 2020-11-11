529c3a50c4024f72ff45408d81c16873b6b29531
Kristoffer Steen <kristoffer.steen@bouvet.no>
2020-11-11 09:46:19 +0100

    Feature/sandbox add dataset (#390)
    * refactor: renamed controllers
    
    instead of StudyController_participant, now called StudyParticipantController
    
    * feat(study): implemented endpoint that returns all datasets added to study
    
    includes general and study specific
    
    * fix: changed IDatasetService getDatasetssForstudy to IEnumerable
    
    * feat(sandbox): can now add, remove and list datasets
    
    * fix(study participant): fixed url back to how it was before, ended up changing url in a refactor
    
    * fix(study participant): changed the search endpoint url, removed /study/ part of url again
    
    * fix(study participant): changed to studies, not study in urls
    
    Co-authored-by: Hans Kristian Vik Olsen <hanol@equinor.com>

---
+++ src/Sepes.RestApi/Controllers/StudyParticipantController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Sepes.Infrastructure.Constants;
+using Sepes.Infrastructure.Dto;
+using Sepes.Infrastructure.Service.Interface;
+using System.Threading.Tasks;
+
+namespace Sepes.RestApi.Controller
+{
+    [Route("api/")]
+    [ApiController]
+    [Produces("application/json")]
+    [EnableCors("_myAllowSpecificOrigins")]
+    [Authorize(Roles = AppRoles.Admin)] //Todo: Need wider access, but restricted for now
+    public class StudyParticipantController : ControllerBase
+    {
+        readonly IStudyParticipantService _studyParticipantService;
+
+        public StudyParticipantController(IStudyParticipantService studyParticipantService)
+        {
+            _studyParticipantService = studyParticipantService;
+        }
+
+        //Get list of lookup items
+        [HttpGet("participants")]
+        public async Task<IActionResult> GetLookupAsync(string search)
+        {
+            var studies = await _studyParticipantService.GetLookupAsync(search);
+            return new JsonResult(studies);
+        }
+
+        [HttpPut("studies/{studyId}/participants/{role}")]
+        public async Task<IActionResult> AddParticipantAsync(int studyId, ParticipantLookupDto user, string role)
+        {
+            var updatedStudy = await _studyParticipantService.HandleAddParticipantAsync(studyId, user, role);
+            return new JsonResult(updatedStudy);
+        }    
+
+        [HttpDelete("studies/{studyId}/participants/{userId}/{roleName}")]
+        public async Task<IActionResult> RemoveParticipantAsync(int studyId, int userId, string roleName)
+        {
+            var updatedStudy = await _studyParticipantService.RemoveParticipantFromStudyAsync(studyId, userId, roleName);
+            return new JsonResult(updatedStudy);
+        }
+    }
+}
