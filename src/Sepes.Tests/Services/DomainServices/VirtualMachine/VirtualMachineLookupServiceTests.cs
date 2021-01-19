94886e74937ebf7528af3834cdeb1beba69a3d17
Hans Kristian Vik Olsen <35257765+HansKristianVikOlsen@users.noreply.github.com>
2021-01-19 14:41:57 +0100

    test(VirtualMachinelookupService): new test for vm name and username (#489)
    * test(VirtualMachinelookupService): new test for vm name and username
    
    Closes #487

---
+++ src/Sepes.Tests/Services/DomainServices/VirtualMachine/VirtualMachineLookupServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Sepes.Tests.Setup;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Sepes.Tests.Services.DomainServices.VirtualMachine
+{
+    public class VirtualMachineLookupServiceTests: VirtualMachineLookupServiceBase
+    {
+        public VirtualMachineLookupServiceTests()
+           : base()
+        {
+
+        }
+
+
+        [Theory]
+        [InlineData("admin", false)]
+        [InlineData("admin123", true)]
+        [InlineData("admin123.", false)]
+        public void GetVirtualMachineUserNameValdiation(string name, Boolean expectedResult)
+        {
+            var virtualMachineLookupService = VirtualMachineMockFactory.GetVirtualMachineLookupService(_serviceProvider);
+            var validationOfName = virtualMachineLookupService.CheckIfUsernameIsValidOrThrow(name);
+
+            Assert.Equal(validationOfName.isValid, expectedResult);
+        }
+
+        [Theory]
+        [InlineData("study1", "sandbox1", "james", "vm-study1-sandbox1-james")]
+        public void GetVirtualMachineVMNameValdiation(string studyName, string sandboxName, string prefix, string expectedResult)
+        {
+            var virtualMachineLookupService = VirtualMachineMockFactory.GetVirtualMachineLookupService(_serviceProvider);
+            var calculatedName = virtualMachineLookupService.CalculateName(studyName, sandboxName, prefix);
+
+            Assert.Equal(calculatedName, expectedResult);
+        }
+    }
+}
---
+++ src/Sepes.Tests/Setup/VirtualMachineMockFactory.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Sepes.Infrastructure.Dto.Sandbox;
+using Sepes.Infrastructure.Model.Context;
+using Sepes.Infrastructure.Service;
+using Sepes.Infrastructure.Service.Azure.Interface;
+using Sepes.Infrastructure.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sepes.Tests.Setup
+{
+    public class VirtualMachineMockFactory
+    {
+        public static IVirtualMachineLookupService GetVirtualMachineLookupService(ServiceProvider serviceProvider)
+        {
+            var db = serviceProvider.GetService<SepesDbContext>();
+            var mapper = serviceProvider.GetService<IMapper>();
+            var logger = serviceProvider.GetService<ILogger<VirtualMachineService>>();
+            var userService = UserFactory.GetUserServiceMockForAdmin(1);
+
+            var sandboxServiceMock = new Mock<ISandboxService>();
+            sandboxServiceMock.Setup(x => x.CreateAsync(1, It.IsAny<SandboxCreateDto>()));
+
+            var costServiceMock = new Mock<IAzureCostManagementService>();
+            sandboxServiceMock.Setup(x => x.CreateAsync(1, It.IsAny<SandboxCreateDto>()));
+
+            return new VirtualMachineLookupService(logger, db, mapper, sandboxServiceMock.Object, costServiceMock.Object);
+        }
+    }
+}
