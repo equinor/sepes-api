21db4098d9bbfbfeb2a34ed251568da8ab2a7ca0
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-01-20 12:57:35 +0100

    test: for VirtualMachine password and VirtualMachineSize calculateVmCost
    Closes #486

---
+++ src/Sepes.Tests/Services/DomainServices/VirtualMachine/VirtualMachineSizeServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using Sepes.Infrastructure.Dto.VirtualMachine;
+using Sepes.Infrastructure.Service.Interface;
+using Sepes.Tests.Setup;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Sepes.Infrastructure.Constants.Auth;
+using Sepes.Infrastructure.Dto.Sandbox;
+using Sepes.Infrastructure.Constants;
+
+namespace Sepes.Tests.Services.DomainServices.VirtualMachine
+{
+    public class VirtualMachineSizeServiceTests : VirtualMachineSizeServiceBase
+    {
+        public VirtualMachineSizeServiceTests()
+           : base()
+        {
+        }
+            [Fact]
+            public async void GetCalculateVmPrice_ShouldReturnPrice()
+            {
+                await GetCalculateVmPrice_ShouldReturnPrice_RefreshAndSeedTestDatabase();
+                var virtualMachineSizeService = VirtualMachineMockFactory.GetVirtualMachineSizeService(_serviceProvider);
+                var priceOfVm = await virtualMachineSizeService.CalculateVmPrice(1, new CalculateVmPriceUserInputDto { Size = "Size1", DataDisks=new List<string>(new string[] { "Disk1"}), OperatingSystem="windows" });
+
+                Assert.Equal("15", priceOfVm.ToString());
+            }
+
+    }
+}
+
