127aaa8a59da868dc301259084123bd29ae9af04
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-01-21 11:59:55 +0100

    fix(participants): issue where sepes was dependent on Azure to return users. Now it will return db users that match search, regardless of it receives users from azure
    Closes #499

---
+++ src/Sepes.Tests/Services/DomainServices/StudyParticipant/StudyParticipantLookupServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using Sepes.Tests.Setup;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq
+
+namespace Sepes.Tests.Services.DomainServices.StudyParticipant
+{
+    public class StudyParticipantLookupServiceTests : StudyParticipantLookupBase
+    {
+        public StudyParticipantLookupServiceTests()
+           : base()
+        {
+
+        }
+
+        [Fact]
+        public async void GetParticipantsWithNameFrom_GetLookupAsync()
+        {
+            await RefreshAndSeedTestDatabase();
+            var studyParticipantLookupService = StudyParticipantMockFactory.GetStudyParticipantLookupService(_serviceProvider);
+            var usersWithName = await studyParticipantLookupService.GetLookupAsync("John");
+            
+            Assert.Equal(2, usersWithName.Count());
+        }
+
+        [Fact]
+        public async void GetParticipantsWithEmailFrom_GetLookupAsync()
+        {
+            await RefreshAndSeedTestDatabase();
+            var studyParticipantLookupService = StudyParticipantMockFactory.GetStudyParticipantLookupService(_serviceProvider);
+            var usersWithName = await studyParticipantLookupService.GetLookupAsync("John@hotmail.com");
+            Assert.Equal(1, usersWithName.Count());
+        }
+
+        [Fact]
+        public async void GetInvalidNameParticipantsFrom_GetLookupAsync()
+        {
+            await RefreshAndSeedTestDatabase();
+            var studyParticipantLookupService = StudyParticipantMockFactory.GetStudyParticipantLookupService(_serviceProvider);
+            var usersWithName = await studyParticipantLookupService.GetLookupAsync("No Person Has this name");
+            Assert.Equal(0, usersWithName.Count());
+        }
+    }
+}
---
+++ src/Sepes.Tests/Setup/StudyParticipantMockFactory.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Sepes.Infrastructure.Model.Context;
+using Sepes.Infrastructure.Service;
+using Sepes.Infrastructure.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sepes.Tests.Setup
+{
+    public class StudyParticipantMockFactory
+    {
+        public static IStudyParticipantLookupService GetStudyParticipantLookupService(ServiceProvider serviceProvider)
+        {
+            var db = serviceProvider.GetService<SepesDbContext>();
+            var mapper = serviceProvider.GetService<IMapper>();
+            var logger = serviceProvider.GetService<ILogger<StudyParticipantLookupService>>();
+            var userService = UserFactory.GetUserServiceMockForAdmin(1);
+
+            var azureUserService = new Mock<IAzureUserService>();
+
+            var provisioningQueueService = new Mock<IProvisioningQueueService>();
+
+            var cloudResourceOperationCreateService = new Mock<ICloudResourceOperationCreateService>();
+
+            var cloudResourceOperationUpdateService = new Mock<ICloudResourceOperationUpdateService>();
+
+            return new StudyParticipantLookupService(db, logger, mapper, userService.Object, azureUserService.Object, provisioningQueueService.Object,
+                cloudResourceOperationCreateService.Object, cloudResourceOperationUpdateService.Object);
+        }
+    }
+}
