fc0577f31478599862dc02b6ef4916e0cd63767f
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-01-21 15:48:37 +0100

    fix(vm): issue with password validation: Updated test
    Closes #504
    Closes #503

--- src/Sepes.Tests/Services/DomainServices/VirtualMachine/VirtualMachineServiceTests.cs
+++ src/Sepes.Tests/Services/DomainServices/VirtualMachine/VirtualMachineServiceTests.cs
@@ -25,5 +25,13 @@ namespace Sepes.Tests.Services.DomainServices.VirtualMachine
 
             await Assert.ThrowsAsync<System.Exception>(async () => await validationOfName);
         }
+
+        [Fact]
+        public async void CheckvalidPassword()
+        {
+            var validPassword = "aA!1aaaaaaaa";
+            var virtualMachineLookupService = VirtualMachineMockFactory.GetVirtualMachineService(_serviceProvider);
+            virtualMachineLookupService.ValidateVmPasswordOrThrow(validPassword);
+        }
     }
 }
