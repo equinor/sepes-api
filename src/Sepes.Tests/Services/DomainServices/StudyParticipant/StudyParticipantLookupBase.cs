f34f980bcd635de0832ab4f2b0ef42dfda7e7448
Hans Kristian Vik Olsen <35257765+HansKristianVikOlsen@users.noreply.github.com>
2021-01-22 09:17:53 +0100

    fix(participants): issue where sepes was dependent on Azure to return… (#502)
    * fix(participants): issue where sepes was dependent on Azure to return users. Now it will return db users that match search, regardless of it receives users from azure
    
    Closes #499

---
+++ src/Sepes.Tests/Services/DomainServices/StudyParticipant/StudyParticipantLookupBase.cs
@@ -0,0 +1,38 @@
+﻿using Sepes.Infrastructure.Model;
+using Sepes.Infrastructure.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sepes.Tests.Services.DomainServices.StudyParticipant
+{
+    public class StudyParticipantLookupBase : ServiceTestBase
+    {
+        protected async Task<SepesDbContext> RefreshAndSeedTestDatabase()
+        {
+            var db = await ClearTestDatabase();
+
+            var user = new User()
+            {
+                Id = 1,
+                FullName = "John Doe",
+                EmailAddress = "John@hotmail.com",
+                ObjectId = "1"
+            };
+
+            var user2 = new User()
+            {
+                Id = 2,
+                FullName = "John Doe2",
+                EmailAddress = "John2@hotmail.com",
+                ObjectId = "2"
+            };
+
+            db.Users.Add(user);
+            db.Users.Add(user2);
+            await db.SaveChangesAsync();
+            return db;
+        }
+    }
+}
