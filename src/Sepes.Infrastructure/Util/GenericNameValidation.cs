cbb546cf600274ca5b90661913af2255285f23c3
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-01-25 08:21:30 +0100

    wip: changed to argumentException

--- src/Sepes.Infrastructure/Util/GenericNameValidation.cs
+++ src/Sepes.Infrastructure/Util/GenericNameValidation.cs
@@ -11,14 +11,14 @@ namespace Sepes.Infrastructure.Util
         {
             if (string.IsNullOrWhiteSpace(name))
             {
-                throw new Exception("Name can not be empty");
+                throw new ArgumentException("Name can not be empty");
             }
 
             var onlyLettersAndNumbers = new Regex(@"^[a-zA-Z0-9]+$");
 
             if(!onlyLettersAndNumbers.IsMatch(name) || name.Length < minimumLength)
             {
-                throw new Exception("Name should should only contain letters or/and numbers and be minimum 3 characters long");
+                throw new ArgumentException("Name should should only contain letters or/and numbers and be minimum 3 characters long");
             }
         }
     }
