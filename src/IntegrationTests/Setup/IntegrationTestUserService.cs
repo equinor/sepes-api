3fac8343d2c7780bdfbb62f270be33a6649e1bb9
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-01-25 15:40:43 +0100

    test: add webApplicationFactory to sepes
    Closes #506

---
+++ src/IntegrationTests/Setup/IntegrationTestUserService.cs
@@ -0,0 +1,31 @@
+﻿using Sepes.Infrastructure;
+using Sepes.Infrastructure.Interface;
+using System;
+using Sepes.RestApi.IntegrationTests.TestHelpers;
+
+namespace Sepes.IntegrationTests.Setup
+{
+    public class IntegrationTestUserService : ICurrentUserService
+    {
+
+        public string GetUserFullName()
+        {
+            return "Mr Integration Test";
+        }
+
+        public string GetUserGuid()
+        {
+            return TestConstants.TestUserGuid;
+        }
+
+        public string GetUserId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUsername()
+        {
+            return "test_user@integrationtesting.com";
+        }
+    }
+}
