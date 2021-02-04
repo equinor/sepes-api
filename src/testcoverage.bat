b62b60de34d99967c0978bbdb6062075e0780a41
krsteen <kristoffer.steen@hotmail.com>
2021-02-04 12:03:58 +0100

    wip: changed report format. added runsettings file

--- src/testcoverage.bat
+++ src/testcoverage.bat
@@ -1,7 +1,8 @@
 @echo off
 dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools
-dotnet test .\Sepes.RestApi.IntegrationTests\ /p:CollectCoverage=true /p:CoverletOutput=../CodeCoverage/IntegrationTest /p:CoverletOutputFormat=cobertura 
+rem dotnet test .\Sepes.RestApi.IntegrationTests\ /p:CollectCoverage=true /p:CoverletOutput=../CodeCoverage/IntegrationTest /p:CoverletOutputFormat=cobertura 
 dotnet test .\Sepes.Tests\ /p:CollectCoverage=true /p:CoverletOutput=../CodeCoverage/UnitTest /p:CoverletOutputFormat=cobertura 
-tools\reportgenerator.exe -reports:CodeCoverage/**/UnitTest.cobertura.xml;CodeCoverage/**/IntegrationTest.cobertura.xml -targetdir:./CodeCoverage/Report -reporttypes:HTML;HTMLSummary
+rem tools\reportgenerator.exe -reports:CodeCoverage/**/UnitTest.cobertura.xml;CodeCoverage/**/IntegrationTest.cobertura.xml -targetdir:./CodeCoverage/Report -reporttypes:HtmlInline
+tools\reportgenerator.exe -reports:CodeCoverage/**/UnitTest.cobertura.xml -targetdir:./CodeCoverage/Report -reporttypes:HtmlSummary
 start chrome ./CodeCoverage/Report/index.htm
 pause
