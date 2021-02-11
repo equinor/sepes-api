79463dc7eb3c4a62aabd786295f6d419e3bf3a33
krsteen <kristoffer.steen@hotmail.com>
2021-02-11 09:42:33 +0100

    chore: improved health endpoint

--- src/Sepes.Infrastructure/Service/HealthService.cs
+++ src/Sepes.Infrastructure/Service/HealthService.cs
@@ -8,6 +8,8 @@ using Sepes.Infrastructure.Service.Interface;
 using Sepes.Infrastructure.Util;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -59,20 +61,45 @@ namespace Sepes.Infrastructure.Service
                 var remoteIpAddress = context.Connection.RemoteIpAddress;
                 var localIpAddress = context.Connection.LocalIpAddress;
 
-                return new Dictionary<string, string>()
-                {
+                var response = new Dictionary<string, string>()
+                     {
                     { "Server public ip from 3rd party", await IpAddressUtil.GetServerPublicIp(_config) },
                     { "context.Connection.RemoteIpAddress", remoteIpAddress.ToString() },
                     { "context.Connection.RemoteIpAddress MapToIPv4", remoteIpAddress.MapToIPv4().ToString() },
                     { "context.Connection.RemoteIpAddress MapToIPv6", remoteIpAddress.MapToIPv6().ToString() },
                     { "context.Connection.RemoteIpAddress AddressFamily", remoteIpAddress.AddressFamily.ToString() },
-
                     { "context.Connection.LocalIpAddress", localIpAddress.ToString() },
                     { "context.Connection.LocalIpAddress MapToIPv4", localIpAddress.MapToIPv4().ToString() },
                     { "context.Connection.LocalIpAddress MapToIPv6", localIpAddress.MapToIPv6().ToString() },
                     { "context.Connection.LocalIpAddress AddressFamily", localIpAddress.AddressFamily.ToString() },
 
                 };
+
+                var counter = 0;
+
+                foreach (var curDns in Dns.GetHostEntry(remoteIpAddress).AddressList)
+                {
+                    try
+                    {
+                        var description = $"{curDns} | family: {curDns.AddressFamily}";
+
+                        if (curDns.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+                        {
+                            description += $" | v4: {curDns.MapToIPv4()}";
+                        }
+
+                        response.Add($"fromDns[{counter}]", description);
+                        counter++;
+                    }
+                    catch (Exception)
+                    {
+
+                     
+                    }
+                 
+                }
+
+                return response;
             }
             catch (Exception ex)
             {
