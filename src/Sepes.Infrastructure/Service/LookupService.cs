673283b7980f6a60e0dbdbc93faae2cdfdeb1e3a
Hans Kristian Vik Olsen <35257765+HansKristianVikOlsen@users.noreply.github.com>
2021-02-16 12:49:18 +0100

    fix: New endpoint for study roles where it will return which role a u… (#560)
    * fix: New endpoint for study roles where it will return which role a user can assign to another user
    
    Co-authored-by: krsteen <kristoffer.steen@hotmail.com>

--- src/Sepes.Infrastructure/Service/LookupService.cs
+++ src/Sepes.Infrastructure/Service/LookupService.cs
@@ -1,17 +1,27 @@
 ﻿using AutoMapper;
 using Sepes.Infrastructure.Dto;
+using Sepes.Infrastructure.Model.Context;
+using Sepes.Infrastructure.Service.DataModelService.Interface;
 using Sepes.Infrastructure.Service.Interface;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Sepes.Infrastructure.Service
 {
     public class LookupService : ILookupService
     {
+        public SepesDbContext _db;
         public IMapper _mapper;
+        public IUserService _userService;
+        public IStudyModelService _studyModelService;
 
-        public LookupService(IMapper mapper)
+        public LookupService(SepesDbContext db, IMapper mapper, IUserService userService, IStudyModelService studyModelService)
         {
             _mapper = mapper;
+            _userService = userService;
+            _db = db;
+            _studyModelService = studyModelService;
         }     
 
         public IEnumerable<LookupDto> StudyRoles()
@@ -23,6 +33,42 @@ namespace Sepes.Infrastructure.Service
                 new LookupDto{ Key= Constants.StudyRoles.VendorContributor,  DisplayValue = Constants.StudyRoles.VendorContributor },
                 new LookupDto{ Key= Constants.StudyRoles.StudyViewer,  DisplayValue =  Constants.StudyRoles.StudyViewer }
             };
+        }
+
+        public async Task<IEnumerable<LookupDto>> StudyRolesUserCanGive(int studyId)
+        {
+            var user = await _userService.GetCurrentUserAsync();
+            var studyFromDb = await _studyModelService.GetByIdWithoutPermissionCheckAsync(studyId, true, true);
+                    
+            var existingParticipantRoles = studyFromDb.StudyParticipants.Where(x => x.UserId == user.Id).ToList();
+            var result = new Dictionary<string, LookupDto>();
+
+            foreach (var curExistingRole in existingParticipantRoles)
+            {
+                if (result.ContainsKey(curExistingRole.RoleName))
+                {
+                    continue;
+                }
+             
+                if (curExistingRole.RoleName == Constants.StudyRoles.VendorContributor)
+                {
+                    result.Add(Constants.StudyRoles.VendorContributor, new LookupDto(Constants.StudyRoles.VendorContributor));
+                }
+                else if (curExistingRole.RoleName == Constants.StudyRoles.VendorAdmin)
+                {
+                    result.Add(Constants.StudyRoles.VendorAdmin, new LookupDto(Constants.StudyRoles.VendorAdmin));
+                    result.Add(Constants.StudyRoles.VendorContributor, new LookupDto(Constants.StudyRoles.VendorContributor));
+                }
+                else if (curExistingRole.RoleName == Constants.StudyRoles.SponsorRep || curExistingRole.RoleName == Constants.StudyRoles.StudyOwner)
+                {                   
+                    result.Add(Constants.StudyRoles.SponsorRep, new LookupDto(Constants.StudyRoles.SponsorRep));
+                    result.Add(Constants.StudyRoles.VendorAdmin, new LookupDto(Constants.StudyRoles.VendorAdmin));
+                    result.Add(Constants.StudyRoles.VendorContributor, new LookupDto(Constants.StudyRoles.VendorContributor));
+                    result.Add(Constants.StudyRoles.StudyViewer, new LookupDto(Constants.StudyRoles.StudyViewer));
+                }               
+            }
+
+            return result.Values.ToList();
         }
     }
 }
