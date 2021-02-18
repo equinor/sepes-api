50043e8c2e7cc64983acd20cbb55094ab5d5ac58
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-02-18 10:29:38 +0100

    test: AzureResourceTagsFactory test

---
+++ src/Sepes.Tests/Setup/AzureResourceTagsFactory_Factory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sepes.Tests.Setup
+{
+    public class AzureResourceTagsFactory_Factory
+    {
+        public static IConfiguration GetConfiguration(ServiceProvider serviceProvider)
+        {
+            var config = serviceProvider.GetService<IConfiguration>();
+
+            return config;
+        }
+    }
+}
---
+++ src/Sepes.Tests/Util/AzureResourceTagsFactoryTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sepes.Infrastructure.Model;
+using Sepes.Infrastructure.Util;
+using Sepes.Tests.Services;
+using Sepes.Tests.Setup;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Sepes.Tests.Util
+{
+    public class AzureResourceTagsFactoryTest : ServiceTestBase
+    {
+        [Fact]
+        public void VmRule_IsSameRule_withSameRule_shouldBeTrue()
+        {
+
+            var config = AzureResourceTagsFactory_Factory.GetConfiguration(_serviceProvider);
+            List<StudyParticipant> participants = new List<StudyParticipant>();
+            participants.Add(new StudyParticipant() { RoleName = "Study Owner" });
+            var study = new Study() { Name = "Study1", WbsCode = "123", StudyParticipants = participants };
+            var sandbox = new Sandbox() { Study = study, Name = "Sandbox1" };
+
+            var res = AzureResourceTagsFactory.SandboxResourceTags(config, study, sandbox);
+
+            Dictionary<string, string> expectedResult = new Dictionary<string, string>();
+            expectedResult.Add("txt", "notepad.exe");
+
+            Assert.Equal(expectedResult, res);
+        }
+    }
+}
