7641a0776d47678963d33fc599c80ce451e873a3
krsteen <kristoffer.steen@hotmail.com>
2021-02-23 09:19:57 +0100

    wip: separate runsettings for each test project

--- src/testcoverage.bat
+++ src/testcoverage.bat
@@ -2,8 +2,8 @@
 dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools
 rm -r %~dp0\Sepes.RestApi.IntegrationTests\TestResults\
 rm -r %~dp0\Sepes.Tests\TestResults\
-dotnet test .\Sepes.RestApi.IntegrationTests\ --collect:"XPlat Code Coverage" --settings coverage.runsettings
-dotnet test .\Sepes.Tests\ --collect:"XPlat Code Coverage" --settings coverage.runsettings
+dotnet test .\Sepes.RestApi.IntegrationTests\ --collect:"XPlat Code Coverage" --settings .\Sepes.RestApi.IntegrationTests\integrationtests.runsettings
+dotnet test .\Sepes.Tests\ --collect:"XPlat Code Coverage" --settings .\Sepes.Tests\unittests.runsettings
 tools\reportgenerator.exe -reports:Sepes.Tests/TestResults/**/coverage.cobertura.xml;Sepes.RestApi.IntegrationTests/TestResults/**/coverage.cobertura.xml -targetdir:./CodeCoverage/Report -reporttypes:HtmlSummary
 start chrome ./CodeCoverage/Report/summary.htm
 pause
