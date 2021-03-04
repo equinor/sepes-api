4188138bc873e6cb259a101e783c0ce3c1e6e1e0
krsteen <kristoffer.steen@hotmail.com>
2021-03-04 17:58:01 +0100

    fix(dataset): better handling of missing resource group edge case

--- src/Sepes.Infrastructure/Service/Dataset/DatasetCloudResourceService.cs
+++ src/Sepes.Infrastructure/Service/Dataset/DatasetCloudResourceService.cs
@@ -79,15 +79,71 @@ namespace Sepes.Infrastructure.Service
             await _provisioningQueueService.SendMessageAsync(parentQueueItem, cancellationToken: cancellationToken);
         }
 
+
         public async Task CreateResourcesForStudySpecificDatasetAsync(Dataset dataset, string clientIp, CancellationToken cancellationToken = default)
         {
             _logger.LogInformation($"CreateResourcesForStudySpecificDataset - Dataset Id: {dataset.Id}");
 
             var parentQueueItem = QueueItemFactory.CreateParent("Create resources for Study Specific Dataset");
-            var resourceGroupDb = GetResourceGroupForStudySpecificDataset(dataset.Study);
+            var resourceGroupDb = await EnsureResourceGroupExists(dataset, parentQueueItem);
+
             await OrderCreationOfStudySpecificDatasetStorageAccount(dataset, resourceGroupDb, clientIp, parentQueueItem, cancellationToken);
 
             await _provisioningQueueService.SendMessageAsync(parentQueueItem, cancellationToken: cancellationToken);
+        }
+
+        async Task<CloudResource> EnsureResourceGroupExists(Dataset dataset, ProvisioningQueueParentDto parentQueueItem)
+        {
+            try
+            {
+                var resourceGroupDb = GetResourceGroupForStudySpecificDataset(dataset.Study);
+
+                if (resourceGroupDb == null)
+                {
+                    resourceGroupDb = await CreateResourceGroupForStudySpecificDatasetsInternalAsync(dataset.Study, parentQueueItem);
+                }
+                else
+                {
+                    if (String.IsNullOrWhiteSpace(resourceGroupDb.LastKnownProvisioningState))
+                    {
+                        var resourceGroupCreateOperation = CloudResourceOperationUtil.GetCreateOperation(resourceGroupDb);
+
+                        //Old and failed, give it another try
+                        if (resourceGroupCreateOperation != null
+                            && resourceGroupCreateOperation.Created.AddMinutes(10) <= DateTime.UtcNow
+                            && (resourceGroupCreateOperation.Status == CloudResourceOperationState.FAILED || resourceGroupCreateOperation.Status == CloudResourceOperationState.ABORTED))
+                        {
+                            ProvisioningQueueUtil.CreateChildAndAdd(parentQueueItem, resourceGroupDb);
+                        }
+                    }
+                    else
+                    {
+                        if (resourceGroupDb.LastKnownProvisioningState == CloudResourceProvisioningStates.SUCCEEDED || resourceGroupDb.LastKnownProvisioningState == CloudResourceProvisioningStates.CREATING)
+                        {
+                            return resourceGroupDb;
+                        }
+                    }
+                }
+
+                return resourceGroupDb;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to locate or ensure that resource group exist for storage account", ex);
+            }
+        }
+
+        async Task<CloudResource> CreateResourceGroupForStudySpecificDatasetsInternalAsync(Study study, ProvisioningQueueParentDto parentQueueItem)
+        {
+            var resourceGroupName = AzureResourceNameUtil.StudySpecificDatasetResourceGroup(study.Name);
+            var tags = AzureResourceTagsFactory.StudySpecificDatasourceResourceGroupTags(_config, study);
+            var resourceGroupForDatasets = await _cloudResourceCreateService.CreateStudySpecificResourceGroupEntryAsync(study.Id, resourceGroupName, "norwayeast", tags);
+            ProvisioningQueueUtil.CreateChildAndAdd(parentQueueItem, resourceGroupForDatasets);
+
+            await ScheduleResourceGroupRoleAssignments(study, resourceGroupForDatasets, parentQueueItem);
+
+            return resourceGroupForDatasets;
         }
 
         CloudResource GetResourceGroupForStudySpecificDataset(Study study, bool includeDeleted = false)
@@ -131,6 +187,11 @@ namespace Sepes.Infrastructure.Service
         {
             try
             {
+                if (resourceGroup == null)
+                {
+                    throw new ArgumentNullException("resourceGroup", "Resource group entry is null");
+                }
+
                 _logger.LogInformation($"CreateResourcesForStudySpecificDataset - Dataset Id: {dataset.Id}");
 
                 var currentUser = await _userService.GetCurrentUserAsync();
@@ -164,7 +225,7 @@ namespace Sepes.Infrastructure.Service
             {
                 throw new Exception($"Failed to schedule creation of Azure Storage Account", ex);
             }
-        }       
+        }
 
         public async Task EnsureFirewallExistsAsync(Study study, Dataset dataset, string clientIp, CancellationToken cancellationToken = default)
         {
@@ -183,8 +244,8 @@ namespace Sepes.Infrastructure.Service
 
                 await ProvisioningQueueUtil.CreateItemAndEnqueue(_provisioningQueueService, firewallUpdateOperation);
 
-                await OperationCompletedUtil.WaitForOperationToCompleteAsync(_cloudResourceOperationReadService, firewallUpdateOperation.Id);            
-            } 
+                await OperationCompletedUtil.WaitForOperationToCompleteAsync(_cloudResourceOperationReadService, firewallUpdateOperation.Id);
+            }
         }
 
         public async Task DeleteAllStudyRelatedResourcesAsync(Study study, CancellationToken cancellationToken = default)
