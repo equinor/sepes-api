d510dedf6805456171cd696bce61d9590d91a7cd
krsteen <kristoffer.steen@hotmail.com>
2021-03-10 12:37:17 +0100

    chore: couple of new indexes for study, sb and resource

---
+++ src/Sepes.Infrastructure/Migrations/20210310112329_MoreIndex.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sepes.Infrastructure.Migrations
+{
+    public partial class MoreIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CloudResources_SandboxId",
+                table: "CloudResources");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CloudResources_StudyId",
+                table: "CloudResources");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CloudResources_SandboxId_ResourceName",
+                table: "CloudResources",
+                columns: new[] { "SandboxId", "ResourceName" },
+                filter: "[Deleted] = 0")
+                .Annotation("SqlServer:Include", new[] { "Id", "Region" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CloudResources_StudyId_ResourceName",
+                table: "CloudResources",
+                columns: new[] { "StudyId", "ResourceName" },
+                filter: "[Deleted] = 0")
+                .Annotation("SqlServer:Include", new[] { "Id", "Region" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CloudResources_SandboxId_ResourceName",
+                table: "CloudResources");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CloudResources_StudyId_ResourceName",
+                table: "CloudResources");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CloudResources_SandboxId",
+                table: "CloudResources",
+                column: "SandboxId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CloudResources_StudyId",
+                table: "CloudResources",
+                column: "StudyId");
+        }
+    }
+}
