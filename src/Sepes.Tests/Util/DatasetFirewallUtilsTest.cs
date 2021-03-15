18666447832bbb0c0d8fcd514af345a9448a8349
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-03-15 10:07:35 +0100

    fix: Validate firewall IP rules

--- src/Sepes.Tests/Util/DatasetFirewallUtilsTest.cs
+++ src/Sepes.Tests/Util/DatasetFirewallUtilsTest.cs
@@ -13,9 +13,6 @@ namespace Sepes.Tests.Util
     {
         [Theory]
         [InlineData(2, "1.1.1.1", "1.1.1.2")]
-        [InlineData(1, "", "")]
-        [InlineData(1, null, null)]
-        [InlineData(1, "abc", "dc11")]
         public void SetDatasetFirewallRules_ShouldReturnTrue(int expectedValue, string clientIp, string serverIp)
         {
             var fireWallRules = new List<DatasetFirewallRule>(){ };
@@ -26,6 +23,25 @@ namespace Sepes.Tests.Util
 
             Assert.True(result);
             Assert.Equal(expectedValue, dataset.FirewallRules.Count);
+        }
+
+        [Theory]
+        [InlineData("ClientIp is not an valid IP Address", "", "")]
+        [InlineData("ClientIp is not an valid IP Address", null, null)]
+        [InlineData("ClientIp is not an valid IP Address", "abc", "dc11")]
+        [InlineData("ServerIp is not an valid IP Address", "1.1.1.1", "dc11")]
+        [InlineData("ClientIp is not an valid IP Address", "1a.1.a1.1", "1.1.1.1")]
+        public void SetDatasetFirewallRules_ShouldReturnExceptioon(string expectedResult, string clientIp, string serverIp)
+        {
+            var fireWallRules = new List<DatasetFirewallRule>() { };
+            fireWallRules.Add(new DatasetFirewallRule { Id = 1, Address = "1", Created = DateTime.Today.AddYears(-1) });
+            var user = new UserDto() { Id = 1 };
+            var dataset = new Dataset() { FirewallRules = fireWallRules };
+
+            var ex = Assert.Throws<ArgumentException>(() => DatasetFirewallUtils.SetDatasetFirewallRules(user, dataset, clientIp, serverIp));
+
+            Assert.Equal(expectedResult, ex.Message);
+
         }
     }
 }
