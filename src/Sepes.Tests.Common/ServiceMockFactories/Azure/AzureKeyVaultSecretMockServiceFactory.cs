99a58b8643d72b2160d3c967bab0d1d03879a21c
krsteen <kristoffer.steen@hotmail.com>
2021-03-16 12:18:16 +0100

    wip: fixed wrong uri

--- src/Sepes.Tests.Common/ServiceMockFactories/Azure/AzureKeyVaultSecretMockServiceFactory.cs
+++ src/Sepes.Tests.Common/ServiceMockFactories/Azure/AzureKeyVaultSecretMockServiceFactory.cs
@@ -10,7 +10,7 @@ namespace Sepes.Test.Common.ServiceMockFactories
             var mock = new Mock<IAzureKeyVaultSecretService>();
 
             mock.Setup(us => us.AddKeyVaultSecret(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
-                 .ReturnsAsync(new System.Uri("integrationtest"));
+                 .ReturnsAsync(new System.Uri("https://nonexistenkeyvault.microsoft.net"));
 
             mock.Setup(us => us.DeleteKeyVaultSecretValue(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>()))
                .ReturnsAsync("integrationtest");
