6d93fdfb010bec33e8362a15cd24ab6434f43b95
krsteen <kristoffer.steen@hotmail.com>
2021-03-16 11:02:29 +0100

    wip: splitted integration and unit tests into separate actions

--- src/testcoverage.bat
+++ src/testcoverage.bat
@@ -2,8 +2,13 @@
 dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools
 rm -r %~dp0\Sepes.RestApi.IntegrationTests\TestResults\
 rm -r %~dp0\Sepes.Tests\TestResults\
-dotnet test .\Sepes.RestApi.IntegrationTests\ --collect:"XPlat Code Coverage" --settings .\Sepes.RestApi.IntegrationTests\integrationtests.runsettings
+
 dotnet test .\Sepes.Tests\ --collect:"XPlat Code Coverage" --settings .\Sepes.Tests\unittests.runsettings
-tools\reportgenerator.exe -reports:Sepes.Tests/TestResults/**/coverage.cobertura.xml;Sepes.RestApi.IntegrationTests/TestResults/**/coverage.cobertura.xml -targetdir:./CodeCoverage/Report -reporttypes:HtmlSummary
+tools\reportgenerator.exe -reports:Sepes.Tests/TestResults/**/coverage.cobertura.xml -targetdir:./CodeCoverage/UnitTests -reporttypes:HtmlSummary -title:"Unit Tests"
+
+dotnet test .\Sepes.RestApi.IntegrationTests\ --collect:"XPlat Code Coverage" --settings .\Sepes.RestApi.IntegrationTests\integrationtests.runsettings
+tools\reportgenerator.exe -reports:Sepes.RestApi.IntegrationTests/TestResults/**/coverage.cobertura.xml -targetdir:./CodeCoverage/IntegrationTests -reporttypes:HtmlSummary -title:"Integration Tests"
+
+tools\reportgenerator.exe -reports:Sepes.Tests/TestResults/**/coverage.cobertura.xml;Sepes.RestApi.IntegrationTests/TestResults/**/coverage.cobertura.xml -targetdir:./CodeCoverage/Combined -reporttypes:HtmlSummary -title:"All Tests"
 start chrome ./CodeCoverage/Report/summary.htm
 pause
