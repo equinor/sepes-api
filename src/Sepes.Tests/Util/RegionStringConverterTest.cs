34a29b06feefe95fd3e82e234e0ef1c9f3c96c06
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-03-17 10:24:32 +0100

    test: RegionStringConverter test

---
+++ src/Sepes.Tests/Util/RegionStringConverterTest.cs
@@ -0,0 +1,24 @@
+﻿using Sepes.Infrastructure.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Sepes.Tests.Util
+{
+    public class RegionStringConverterTest
+    {
+        [InlineData("norwayeast", "norwayeast")]
+        [InlineData("westeurope", "europewest")]
+        [InlineData("northeurope", "europenorth")]
+        [InlineData("norwayeast", "")]
+        [InlineData("norwayeast", null)]
+        [InlineData("norwayeast", "abcd")]
+        [Theory]
+        public void Convert_ShouldContainStudyName(string expectedResult, string region)
+        {
+            var result = RegionStringConverter.Convert(region);
+            Assert.Equal(expectedResult, result.Name);
+        }
+    }
+}
