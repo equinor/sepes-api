1b313014271e14aa350f2619826548554ec6338e
Hans Kristian Vik Olsen <35257765+HansKristianVikOlsen@users.noreply.github.com>
2021-03-17 13:10:14 +0100

    test: ValidationUtils test (#625)
    * test: ValidationUtils test

---
+++ src/Sepes.Tests/Util/ValidationUtilsTest.cs
@@ -0,0 +1,36 @@
+﻿using Sepes.Infrastructure.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Sepes.Tests.Util
+{
+    public class ValidationUtilsTest
+    {
+        [Fact]
+        public void ThrowIfValidationErrors_NoErrorsShouldRun()
+        {
+            var errors = new List<string>();
+            ValidationUtils.ThrowIfValidationErrors("prefix", errors);
+        }
+
+        [Fact]
+        public void ThrowIfValidationErrors_NoErrorsShouldThrow()
+        {
+            ValidationUtils.ThrowIfValidationErrors("prefix", null);
+        }
+
+        [Fact]
+        public void ThrowIfValidationErrors_ShouldThrowWithCorrectError()
+        {
+            var errors = new List<string>();
+            errors.Add("Error1");
+            errors.Add("Error2");
+            var ex = Assert.Throws<Exception>(() => ValidationUtils.ThrowIfValidationErrors("prefix", errors));
+            Assert.Contains("prefix", ex.Message);
+            Assert.Contains("Error1", ex.Message);
+            Assert.Contains("Error2", ex.Message);
+        }
+    }
+}
