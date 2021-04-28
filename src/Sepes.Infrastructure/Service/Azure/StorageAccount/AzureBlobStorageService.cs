db73634a13b2b548da2e4ca678242bb57bc34096
Hans Kristian Vik Olsen <35257765+HansKristianVikOlsen@users.noreply.github.com>
2021-04-28 13:44:33 +0200

    fix(dataset): add properties key and last modified to filelist (#655)
    * fix(dataset): add properties key and last modified to filelist

--- src/Sepes.Infrastructure/Service/Azure/StorageAccount/AzureBlobStorageService.cs
+++ src/Sepes.Infrastructure/Service/Azure/StorageAccount/AzureBlobStorageService.cs
@@ -39,7 +39,10 @@ namespace Sepes.Infrastructure.Service.Azure
                 while (await enumerator.MoveNextAsync())
                 {
                     var curBlob = enumerator.Current;
-                    result.Add(new BlobStorageItemDto() { Name = curBlob.Name, ContentType = curBlob.Properties.ContentType, Size = curBlob.Properties.ContentLength.HasValue ? curBlob.Properties.ContentLength.Value : 0 });
+                    if (curBlob.Properties.LastModified.HasValue)
+                    {
+                        result.Add(new BlobStorageItemDto() { Key = curBlob.Name, Name = curBlob.Name, ContentType = curBlob.Properties.ContentType, Size = curBlob.Properties.ContentLength.HasValue ? curBlob.Properties.ContentLength.Value : 0, Modified = curBlob.Properties.LastModified.Value.ToUnixTimeMilliseconds() });
+                    }
                 }
             }
             finally
