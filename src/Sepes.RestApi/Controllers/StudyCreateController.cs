bcfbb44a103f58ad997594714eef6a31bdfd14f8
krsteen <kristoffer.steen@hotmail.com>
2021-05-31 14:23:52 +0200

    wip: studydeletecontroller should

---
+++ src/Sepes.RestApi/Controllers/StudyCreateController.cs
@@ -0,0 +1,37 @@
+﻿using BrunoZell.ModelBinding;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sepes.Common.Dto.Study;
+using Sepes.Infrastructure.Service.Interface;
+using System.Threading.Tasks;
+
+namespace Sepes.RestApi.Controller
+{
+    [Route("api/studies")]
+    [ApiController]
+    [Produces("application/json")]
+    [EnableCors("_myAllowSpecificOrigins")]
+    [Authorize]
+    public class StudyCreateController : ControllerBase
+    {     
+        readonly IStudyCreateService _studyCreateService;   
+       
+
+        public StudyCreateController(IStudyCreateService studyCreateService)
+        {           
+            _studyCreateService = studyCreateService;                     
+        }         
+
+        [HttpPost]
+        public async Task<IActionResult> CreateStudyAsync(
+            [ModelBinder(BinderType = typeof(JsonModelBinder))] StudyCreateDto study,
+            IFormFile image = null)
+        {
+            var createdStudy = await _studyCreateService.CreateAsync(study, image);          
+
+            return new JsonResult(createdStudy);
+        }
+    }
+}
---
+++ src/Sepes.RestApi/Controllers/StudyDeleteController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Sepes.Infrastructure.Service.Interface;
+using System.Threading.Tasks;
+
+namespace Sepes.RestApi.Controller
+{
+    [Route("api/studies")]
+    [ApiController]
+    [Produces("application/json")]
+    [EnableCors("_myAllowSpecificOrigins")]
+    [Authorize]
+    public class StudyDeleteController : ControllerBase
+    {
+        readonly IStudyDeleteService _studyDeleteService;
+
+        public StudyDeleteController(IStudyDeleteService studyDeleteService)
+        {
+            _studyDeleteService = studyDeleteService;           
+        }       
+
+        [HttpDelete("{studyId}")]
+        public async Task<IActionResult> DeleteStudyAsync(int studyId)
+        {
+            await _studyDeleteService.DeleteStudyAsync(studyId);
+            return new NoContentResult();
+        }
+
+        [HttpPut("{studyId}/close")]
+        [Authorize]
+        public async Task<IActionResult> CloseStudyAsync(int studyId)
+        {
+            await _studyDeleteService.CloseStudyAsync(studyId);
+            return new NoContentResult();
+        }              
+    }
+}
