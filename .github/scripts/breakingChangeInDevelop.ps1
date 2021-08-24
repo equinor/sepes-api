22efaeccbc299fac1cbd299614be642ff1f7cf74
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-08-24 09:41:51 +0200

    wip

--- .github/scripts/breakingChangeInDevelop.ps1
+++ .github/scripts/breakingChangeInDevelop.ps1
@@ -1,5 +1,5 @@
 $dateSinceLastCommitToMaster = git log master -1 --format=%ct.
 $output = git log --after=$dateSinceLastCommitToMaster --grep="feat!"
 
-if (!$output) { Write-Host "No breaking change from backend" }
-else { Write-Host "Backend change to be merged to prod" }
+if (!$output) { Write-Host "test2" }
+else { Write-Host "test1" }
