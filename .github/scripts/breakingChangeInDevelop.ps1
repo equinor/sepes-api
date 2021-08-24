5267e6ace99b046bdd881d8607ab56edeb831ea5
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-08-24 11:13:36 +0200

    wip

--- .github/scripts/breakingChangeInDevelop.ps1
+++ .github/scripts/breakingChangeInDevelop.ps1
@@ -1,4 +1,5 @@
 $dateSinceLastCommitToMaster = git log master -1 --format=%ct.
+git checkout build/add_badge_for_breaking_change
 $output = git log --after=$dateSinceLastCommitToMaster --grep="feat!" --grep="fix!" --grep="refactor!"
 
 if (!$output) { return "No breaking change from backend" }
