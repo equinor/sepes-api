8fca100023dde145fab662ced2d5527acde40e5a
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-08-24 10:11:15 +0200

    fix!: test to see if action works

--- .github/scripts/breakingChangeInDevelop.ps1
+++ .github/scripts/breakingChangeInDevelop.ps1
@@ -1,5 +1,5 @@
 $dateSinceLastCommitToMaster = git log master -1 --format=%ct.
-$output = git log --after=$dateSinceLastCommitToMaster --grep="feat!"
+$output = git log --after=$dateSinceLastCommitToMaster --grep="feat!" --grep="fix!" --grep="refactor!"
 
 if (!$output) { return "No breaking change from backend" }
 else { return "Backend change to be merged to prod" }
