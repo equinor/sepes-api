c95203e2a3c29296a122e69f6d97283873c63d34
Bonyfus Martin <bonyfus@easee.no>
2022-02-04 17:06:16 +0100

    refactor: controllers to api endpoint feature folders
    refactor the controllers to api endpoint feature folders for much cleaner code
    
    Closes #800

---
+++ src/Sepes.RestApi/ApiEndpoints/ServiceNow/Create.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Sepes.Common.Dto.ServiceNow;
+using Sepes.Infrastructure.Service.Interface;
+using Sepes.RestApi.ApiEndpoints.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sepes.RestApi.ApiEndpoints.ServiceNow
+{
+    [Route("api/servicenow")]   
+    [EnableCors("_myAllowSpecificOrigins")]
+    [Authorize]
+    public class Create : EndpointBaseAsync.WithRequest<ServiceNowEnquiryCreateDto>.WithActionResult
+    {
+        private readonly IServiceNowApiService _serviceNowApiService;
+        public Create(IServiceNowApiService serviceNowApiService)
+        {
+            _serviceNowApiService = serviceNowApiService;
+        }
+
+        [HttpPost]
+        public override async Task<ActionResult> HandleAsync(ServiceNowEnquiryCreateDto request, CancellationToken cancellationToken = default)
+        {
+            var response = await _serviceNowApiService.CreateEnquiry(request);
+            return new JsonResult(response);
+        }
+    }
+}
