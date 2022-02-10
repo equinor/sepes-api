65feaccbfdc43e3dfaa712c013960c14baff715a
Bonyfus Martin <bonyfus@easee.no>
2022-02-10 17:05:21 +0100

    refactor: sandbox controller refactor
    sandbox controller refactor to api endpoints
    
    Closes #803

--- src/Sepes.RestApi/ApiEndpoints/Base/EndpointBase.cs
+++ src/Sepes.RestApi/ApiEndpoints/Base/EndpointBase.cs
@@ -2,12 +2,77 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sepes.RestApi.ApiEndpoints.Base
 {
-    [ApiController]
-    public abstract class EndpointBase : ControllerBase
+    public static class EndpointBase
     {
+        public static class WithRequest<TRequest>
+        {
+            public abstract class WithResult<TResponse> : ControllerBase
+            {
+                public abstract Task<TResponse> Handle(
+                    TRequest request,
+                    CancellationToken cancellationToken = default
+                );
+            }
+
+            public abstract class WithoutResult : ControllerBase
+            {
+                public abstract Task Handle(
+                    TRequest request,
+                    CancellationToken cancellationToken = default
+                );
+            }
+
+            public abstract class WithActionResult<TResponse> : ControllerBase
+            {
+                public abstract Task<ActionResult<TResponse>> Handle(
+                    TRequest request,
+                    CancellationToken cancellationToken = default
+                );               
+            }
+
+            public abstract class WithActionResult : ControllerBase
+            {
+                public abstract Task<ActionResult> Handle(
+                    TRequest request,
+                    CancellationToken cancellationToken = default
+                );
+            }
+        }
+
+        public static class WithoutRequest
+        {
+            public abstract class WithResult<TResponse> : ControllerBase
+            {
+                public abstract Task<TResponse> Handle(
+                    CancellationToken cancellationToken = default
+                );
+            }
+
+            public abstract class WithoutResult : ControllerBase
+            {
+                public abstract Task Handle(
+                    CancellationToken cancellationToken = default
+                );
+            }
+
+            public abstract class WithActionResult<TResponse> : ControllerBase
+            {
+                public abstract Task<ActionResult<TResponse>> Handle(
+                    CancellationToken cancellationToken = default
+                );
+            }
+
+            public abstract class WithActionResult : ControllerBase
+            {
+                public abstract Task<ActionResult> Handle(
+                    CancellationToken cancellationToken = default
+                );
+            }
+        }
     }
 }
