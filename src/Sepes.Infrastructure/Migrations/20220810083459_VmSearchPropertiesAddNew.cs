9bbe02b1a8406987c1c218116445b7d5f53fd454
Bonyfus Martin <bonm@equinor.com>
2022-08-10 10:59:31 +0200

    feat: add new vm search properties
    add new search properties
    
    Closes #851

---
+++ src/Sepes.Infrastructure/Migrations/20220810083459_VmSearchPropertiesAddNew.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sepes.Infrastructure.Migrations
+{
+    public partial class VmSearchPropertiesAddNew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"SET IDENTITY_INSERT [dbo].[VmImageSearchProperties] ON 
+GO
+INSERT[dbo].[VmImageSearchProperties]([Id], [Created], [CreatedBy], [Publisher], [Offer], [Sku], [DisplayValue], [Category], [PartOfRecommended]) VALUES(11, GETUTCDATE(), N'bonm@equinor.com', N'MicrosoftWindowsServer', N'WindowsServer', N'2022-Datacenter-Core', N'Windows Server Datacenter Core 2022', N'windows', 1)
+INSERT[dbo].[VmImageSearchProperties]([Id], [Created], [CreatedBy], [Publisher], [Offer], [Sku], [DisplayValue], [Category], [PartOfRecommended]) VALUES(12, GETUTCDATE(), N'bonm@equinor.com', N'MicrosoftWindowsServer', N'WindowsServer', N'2022-Datacenter', N'Windows Server Datacenter 2022', N'windows', 1)
+GO
+SET IDENTITY_INSERT[dbo].[VmImageSearchProperties] OFF
+GO");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
